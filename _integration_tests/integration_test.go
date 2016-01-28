4b98e815fb301490f631d318527488ec950f62ea
Ernesto Jiménez <me@ernesto-jimenez.com>
2016-01-28 22:45:16 +0000

    Add integration tests to generate and build code
    These tests would be aimed at finding generating code or with generated code.

---
+++ _integration_tests/integration_test.go
@@ -0,0 +1,47 @@
+package tests
+
+import (
+	"fmt"
+	"go/build"
+	"os/exec"
+	"path"
+	"testing"
+)
+
+func TestBootstrapReadme(t *testing.T) {
+	if err := goagen("./readme", "bootstrap", "-d", "github.com/goadesign/goa/_integration_tests/readme/design"); err != nil {
+		t.Error(err.Error())
+	}
+	if err := gobuild("./readme"); err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func goagen(dir, command string, args ...string) error {
+	pkg, err := build.Import("github.com/goadesign/goa/goagen", "", 0)
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command("go", "run")
+	for _, f := range pkg.GoFiles {
+		cmd.Args = append(cmd.Args, path.Join(pkg.Dir, f))
+	}
+	cmd.Dir = dir
+	cmd.Args = append(cmd.Args, command)
+	cmd.Args = append(cmd.Args, args...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%s\n%s", err.Error(), out)
+	}
+	return nil
+}
+
+func gobuild(dir string) error {
+	cmd := exec.Command("go", "build", "./...")
+	cmd.Dir = dir
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%s\n%s", err.Error(), out)
+	}
+	return nil
+}
