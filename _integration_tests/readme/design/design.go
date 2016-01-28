53a43bff324981c29970b14b20994cc5254e2715
Ernesto Jiménez <me@ernesto-jimenez.com>
2016-01-28 22:45:16 +0000

    Add integration tests to generate and build code
    These tests would be aimed at finding generating code or with generated code.

---
+++ _integration_tests/readme/design/design.go
@@ -0,0 +1,26 @@
+package design
+
+import (
+	. "github.com/goadesign/goa/design"
+	. "github.com/goadesign/goa/design/dsl"
+)
+
+var _ = API("adder", func() {
+	Title("The adder API")
+	Description("A teaser for goa")
+	Host("localhost:8080")
+	Scheme("http")
+})
+
+var _ = Resource("operands", func() {
+	Action("add", func() {
+		Routing(GET("add/:left/:right"))
+		Description("add returns the sum of the left and right parameters in the response body")
+		Params(func() {
+			Param("left", Integer, "Left operand")
+			Param("right", Integer, "Right operand")
+		})
+		Response(OK, "plain/text")
+	})
+
+})
