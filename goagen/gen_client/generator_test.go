679e4c228b49d64f002183b8eb7950c94354f2f9
Raphael Simon <simon.raphael@gmail.com>
2016-02-02 00:02:59 -0800

    Add client default flag value test

--- goagen/gen_client/generator_test.go
+++ goagen/gen_client/generator_test.go
@@ -63,4 +63,55 @@ var _ = Describe("Generate", func() {
 			Ω(err).ShouldNot(HaveOccurred())
 		})
 	})
+
+	Context("with an action with an integer parameter with no default value", func() {
+		BeforeEach(func() {
+			codegen.TempCount = 0
+			design.Design = &design.APIDefinition{
+				APIVersionDefinition: &design.APIVersionDefinition{
+					Name:        "testapi",
+					Title:       "dummy API with no resource",
+					Description: "I told you it's dummy",
+				},
+				Resources: map[string]*design.ResourceDefinition{
+					"foo": &design.ResourceDefinition{
+						Name: "foo",
+						Actions: map[string]*design.ActionDefinition{
+							"show": &design.ActionDefinition{
+								Name: "show",
+								QueryParams: &design.AttributeDefinition{
+									Type: design.Object{
+										"param": &design.AttributeDefinition{Type: design.Integer},
+									},
+								},
+								Routes: []*design.RouteDefinition{
+									&design.RouteDefinition{
+										Verb: "GET",
+										Path: "",
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+			fooRes := design.Design.Resources["foo"]
+			showAct := fooRes.Actions["show"]
+			showAct.Parent = fooRes
+			showAct.Routes[0].Parent = showAct
+		})
+
+		It("generates the correct command flag initialization code", func() {
+			Ω(genErr).Should(BeNil())
+			Ω(files).Should(HaveLen(6))
+			content, err := ioutil.ReadFile(filepath.Join(outDir, "client", "testapi-cli", "commands.go"))
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(len(strings.Split(string(content), "\n"))).Should(BeNumerically(">=", 3))
+			Ω(content).Should(ContainSubstring("var tmp2 int"))
+			Ω(content).Should(ContainSubstring(".Flags()"))
+			_, err = gexec.Build(filepath.Join(testgenPackagePath, "client", "testapi-cli"))
+			Ω(err).ShouldNot(HaveOccurred())
+
+		})
+	})
 })
