79e07d6419e82ee5fd774ee6e665b5fe7552d2f8
Scott Messier <scott@rightscale.com>
2016-07-18 11:52:04 -0700

    added a default length limit of 128 for incoming X-Request-Id header values for safety the issue is that request IDs will appear frequently in logs and the server could be impacted if a bad actor sends very long request IDs

--- middleware/request_id.go
+++ middleware/request_id.go
@@ -13,8 +13,13 @@ import (
 	"golang.org/x/net/context"
 )
 
-// RequestIDHeader is the name of the header used to transmit the request ID.
-const RequestIDHeader = "X-Request-Id"
+const (
+	// RequestIDHeader is the name of the header used to transmit the request ID.
+	RequestIDHeader = "X-Request-Id"
+
+	// DefaultRequestIDLengthLimit is the default maximum length for header value.
+	DefaultRequestIDLengthLimit = 128
+)
 
 // Counter used to create new request ids.
 var reqID int64
@@ -38,11 +43,20 @@ func init() {
 // RequestIDWithHeader behaves like the middleware RequestID, but it takes the request id header
 // as the (first) argument.
 func RequestIDWithHeader(requestIDHeader string) goa.Middleware {
+	return RequestIDWithHeaderAndLengthLimit(requestIDHeader, DefaultRequestIDLengthLimit)
+}
+
+// RequestIDWithHeaderAndLengthLimit behaves like the middleware RequestID, but it takes the
+// request id header as the (first) argument and a length limit for truncation of the request
+// header value if it exceeds a reasonable length. The limit can be negative for unlimited.
+func RequestIDWithHeaderAndLengthLimit(requestIDHeader string, lengthLimit int) goa.Middleware {
 	return func(h goa.Handler) goa.Handler {
 		return func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
 			id := req.Header.Get(requestIDHeader)
 			if id == "" {
 				id = fmt.Sprintf("%s-%d", reqPrefix, atomic.AddInt64(&reqID, 1))
+			} else if lengthLimit >= 0 && len(id) > lengthLimit {
+				id = id[:lengthLimit]
 			}
 			ctx = context.WithValue(ctx, reqIDKey, id)
 
