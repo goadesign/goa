04efef81bd825b66458deba79a5f0695ef959df0
Scott Messier <scott@rightscale.com>
2016-07-18 13:14:59 -0700

    request_id_length_limit (#621)
    * added a default length limit of 128 for incoming X-Request-Id header values for safety
    the issue is that request IDs will appear frequently in logs and the server could be impacted if a bad actor sends very long request IDs
    
    * fixed CR issue

--- middleware/request_id_test.go
+++ middleware/request_id_test.go
@@ -26,7 +26,7 @@ var _ = Describe("RequestID", func() {
 		var err error
 		req, err = http.NewRequest("GET", "/goo", nil)
 		Ω(err).ShouldNot(HaveOccurred())
-		req.Header.Set("X-Request-Id", reqID)
+		req.Header.Set(middleware.RequestIDHeader, reqID)
 		rw = new(testResponseWriter)
 		params = url.Values{"query": []string{"value"}}
 		service.Encoder.Register(goa.NewJSONEncoder, "*/*")
@@ -43,4 +43,52 @@ var _ = Describe("RequestID", func() {
 		Ω(rg(ctx, rw, req)).ShouldNot(HaveOccurred())
 		Ω(middleware.ContextRequestID(newCtx)).Should(Equal(reqID))
 	})
+
+	It("truncates request ID when it exceeds a default limit", func() {
+		var newCtx context.Context
+		h := func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
+			newCtx = ctx
+			return service.Send(ctx, 200, "ok")
+		}
+		tooLong := makeRequestID(2 * middleware.DefaultRequestIDLengthLimit)
+		expected := makeRequestID(middleware.DefaultRequestIDLengthLimit)
+		req.Header.Set(middleware.RequestIDHeader, tooLong)
+		rg := middleware.RequestID()(h)
+		Ω(rg(ctx, rw, req)).ShouldNot(HaveOccurred())
+		Ω(middleware.ContextRequestID(newCtx)).Should(Equal(expected))
+	})
+
+	It("sets the request ID in the context for a custom header and limit", func() {
+		var newCtx context.Context
+		h := func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
+			newCtx = ctx
+			return service.Send(ctx, 200, "ok")
+		}
+		req.Header.Set("Foo", "abcdefghij")
+		rg := middleware.RequestIDWithHeaderAndLengthLimit("Foo", 7)(h)
+		Ω(rg(ctx, rw, req)).ShouldNot(HaveOccurred())
+		Ω(middleware.ContextRequestID(newCtx)).Should(Equal("abcdefg"))
+	})
+
+	It("allows any request ID when length limit is negative", func() {
+		var newCtx context.Context
+		h := func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
+			newCtx = ctx
+			return service.Send(ctx, 200, "ok")
+		}
+		original := makeRequestID(2 * middleware.DefaultRequestIDLengthLimit)
+		req.Header.Set(middleware.RequestIDHeader, string(original))
+		rg := middleware.RequestIDWithHeaderAndLengthLimit(middleware.RequestIDHeader, -1)(h)
+		Ω(rg(ctx, rw, req)).ShouldNot(HaveOccurred())
+		Ω(middleware.ContextRequestID(newCtx)).Should(Equal(string(original)))
+	})
+
 })
+
+func makeRequestID(length int) string {
+	buffer := make([]byte, length)
+	for i := range buffer {
+		buffer[i] = 'x'
+	}
+	return string(buffer)
+}
