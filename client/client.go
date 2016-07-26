54b6e0a15ee3f0799902592862ce1aef33567c17
Dragos Harabor <bits01@users.noreply.github.com>
2016-07-26 16:39:23 -0700

    Request ID is set in ctx and in req header (#647)
    * Request ID is set in ctx and in req header
    
    * Set req header iff ctx has req id

--- client/client.go
+++ client/client.go
@@ -48,11 +48,16 @@ func New(c Doer) *Client {
 // Do wraps the underlying http client Do method and adds logging.
 // The logger should be in the context.
 func (c *Client) Do(ctx context.Context, req *http.Request) (*http.Response, error) {
+	// TODO: setting the request ID should be done via client middleware. For now only set it if the
+	// caller provided one in the ctx.
+	if ctxreqid := ContextRequestID(ctx); ctxreqid != "" {
+		req.Header.Set("X-Request-Id", ctxreqid)
+	}
 	if c.UserAgent != "" {
 		req.Header.Set("User-Agent", c.UserAgent)
 	}
 	startedAt := time.Now()
-	id := shortID()
+	ctx, id := ContextWithRequestID(ctx)
 	goa.LogInfo(ctx, "started", "id", id, req.Method, req.URL.String())
 	if c.Dump {
 		c.dumpRequest(ctx, req)
@@ -195,4 +200,32 @@ func shortID() string {
 	b := make([]byte, 6)
 	io.ReadFull(rand.Reader, b)
 	return base64.StdEncoding.EncodeToString(b)
+}
+
+// clientKey is the private type used to store values in the context.
+// It is private to avoid possible collisions with keys used by other packages.
+type clientKey int
+
+// ReqIDKey is the context key used to store the request ID value.
+const reqIDKey clientKey = 1
+
+// ContextRequestID extracts the Request ID from the context.
+func ContextRequestID(ctx context.Context) string {
+	var reqID string
+	id := ctx.Value(reqIDKey)
+	if id != nil {
+		reqID = id.(string)
+	}
+	return reqID
+}
+
+// ContextWithRequestID returns ctx and the request ID if it already has one or creates and returns a new context with
+// a new request ID.
+func ContextWithRequestID(ctx context.Context) (context.Context, string) {
+	reqID := ContextRequestID(ctx)
+	if reqID == "" {
+		reqID = shortID()
+		ctx = context.WithValue(ctx, reqIDKey, reqID)
+	}
+	return ctx, reqID
 }
