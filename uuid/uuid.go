ded0d8429f1009fe6036847d09431220b0c585a3
Aslak Knutsen <aslak@4fs.no>
2016-07-29 18:50:03 +0200

    Use satori/go.uuid String()

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -5,11 +5,7 @@
 
 package uuid
 
-import (
-	"encoding/hex"
-
-	"github.com/satori/go.uuid"
-)
+import "github.com/satori/go.uuid"
 
 // FromString Wrapper around the real FromString
 func FromString(input string) (UUID, error) {
@@ -28,17 +24,5 @@ const dash byte = '-'
 // Returns canonical string representation of UUID:
 // xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.
 func (u UUID) String() string {
-	buf := make([]byte, 36)
-
-	hex.Encode(buf[0:8], u[0:4])
-	buf[8] = dash
-	hex.Encode(buf[9:13], u[4:6])
-	buf[13] = dash
-	hex.Encode(buf[14:18], u[6:8])
-	buf[18] = dash
-	hex.Encode(buf[19:23], u[8:10])
-	buf[23] = dash
-	hex.Encode(buf[24:], u[10:])
-
-	return string(buf)
+	return uuid.UUID(u).String()
 }
