5aa47cf150718e16415e4193b299013794c28aa4
Taichi Sasaki <tchsskk@gmail.com>
2016-08-17 20:34:23 +0900

    Fix typo about preflight

--- design/definitions.go
+++ design/definitions.go
@@ -157,7 +157,7 @@ type (
 		Methods []string
 		// List of headers exposed to clients
 		Exposed []string
-		// How long to cache a prefligh request response
+		// How long to cache a preflight request response
 		MaxAge uint
 		// Sets Access-Control-Allow-Credentials header
 		Credentials bool
