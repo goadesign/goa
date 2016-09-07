13707e3fee69dada419161d5f4645de6fe551c60
Xander Guzman <xguzman@efolder.net>
2016-09-07 11:18:39 -0600

    Add normalizeKeys() function for normalizing metric key names

--- metrics.go
+++ metrics.go
@@ -3,13 +3,26 @@
 package goa
 
 import (
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/armon/go-metrics"
 )
 
-// metriks is the local instance of metrics.Metrics
-var metriks *metrics.Metrics
+const (
+	allMatcher      string = "*/*"
+	allReplacement  string = "all"
+	normalizedToken string = "_"
+)
+
+var (
+	// metriks is the local instance of metrics.Metrics
+	metriks *metrics.Metrics
+
+	// used for normalizing names by matching '*' and '/' so they can be replaced.
+	invalidCharactersRE = regexp.MustCompile(`[\*/]`)
+)
 
 // NewMetrics initializes goa's metrics instance with the supplied
 // configuration and metrics sink
@@ -24,6 +37,7 @@ func NewMetrics(conf *metrics.Config, sink metrics.MetricSink) (err error) {
 //     AddSample([]string{"my","namespace","key"}, 15.0)
 func AddSample(key []string, val float32) {
 	if metriks != nil {
+		normalizeKeys(key)
 		metriks.AddSample(key, val)
 	}
 }
@@ -33,6 +47,7 @@ func AddSample(key []string, val float32) {
 //     EmitKey([]string{"my","namespace","key"}, 15.0)
 func EmitKey(key []string, val float32) {
 	if metriks != nil {
+		normalizeKeys(key)
 		metriks.EmitKey(key, val)
 	}
 }
@@ -42,6 +57,7 @@ func EmitKey(key []string, val float32) {
 //     IncrCounter([]key{"my","namespace","counter"}, 1.0)
 func IncrCounter(key []string, val float32) {
 	if metriks != nil {
+		normalizeKeys(key)
 		metriks.IncrCounter(key, val)
 	}
 }
@@ -54,6 +70,7 @@ func IncrCounter(key []string, val float32) {
 //    defer MeasureSince([]string{"my","namespace","action}, time.Now())
 func MeasureSince(key []string, start time.Time) {
 	if metriks != nil {
+		normalizeKeys(key)
 		metriks.MeasureSince(key, start)
 	}
 }
@@ -63,6 +80,20 @@ func MeasureSince(key []string, start time.Time) {
 //     SetGauge([]string{"my","namespace"}, 2.0)
 func SetGauge(key []string, val float32) {
 	if metriks != nil {
+		normalizeKeys(key)
 		metriks.SetGauge(key, val)
+	}
+}
+
+// takes in the list of keys and
+func normalizeKeys(key []string) {
+	if key != nil {
+		for i, k := range(key) {
+			// first replace */* with all
+			k = strings.Replace(k, allMatcher, allReplacement, -1)
+
+			// now replace all other invalid characters with a safe one.
+			key[i] = invalidCharactersRE.ReplaceAllString(k, normalizedToken)
+		}
 	}
 }
