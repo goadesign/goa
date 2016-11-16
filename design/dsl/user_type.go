678b215e2e786fb0e9bf59753b11d41f31bf1409
Raphael Simon <simon.raphael@gmail.com>
2016-11-15 21:41:56 -0800

    Add Map key and value validations

--- design/dsl/user_type.go
+++ design/dsl/user_type.go
@@ -61,16 +61,12 @@ func Type(name string, dsl func()) design.UserType {
 // Examples:
 //
 //    var Names = ArrayOf(String, func() {
-//        Pattern("[a-zA-Z]+")
-//    })
-//
-//    var Bottle = Type("bottle", func() {
-//        Attribute("name")
+//        Pattern("[a-zA-Z]+") // Validates elements of the array
 //    })
 //
 //    var Account = Type("Account", func() {
 //        Attribute("bottles", ArrayOf(Bottle), "Account bottles", func() {
-//            MinLength(1)
+//            MinLength(1) // Validates array as a whole
 //        })
 //    })
 //
@@ -111,16 +107,76 @@ func ArrayOf(v interface{}, dsl ...func()) *design.Array {
 //
 // Example:
 //
-//    var Bottle = Type("Bottle", func() {
-//        Attribute("name")
+//    var ReviewByID = MapOf(Int64, String, func() {
+//        Key(func() {
+//            Minimum(1)           // Validates keys of the map
+//        })
+//        Value(func() {
+//            Pattern("[a-zA-Z]+") // Validates values of the map
+//        })
 //    })
 //
 //    var Review = Type("Review", func() {
 //        Attribute("ratings", MapOf(Bottle, Int32), "Bottle ratings")
 //    })
 //
-func MapOf(k, v design.DataType) *design.Map {
-	kat := design.AttributeExpr{Type: k}
-	vat := design.AttributeExpr{Type: v}
-	return &design.Map{KeyType: &kat, ElemType: &vat}
+func MapOf(k, v interface{}, dsl ...func()) *design.Map {
+	var tk, tv design.DataType
+	var ok bool
+	tk, ok = k.(design.DataType)
+	if !ok {
+		if name, ok := k.(string); ok {
+			tk = design.Root.UserType(name)
+		}
+	}
+	tv, ok = v.(design.DataType)
+	if !ok {
+		if name, ok := v.(string); ok {
+			tv = design.Root.UserType(name)
+		}
+	}
+	// never return nil to avoid panics, errors are reported after DSL execution
+	res := &design.Map{KeyType: &design.AttributeExpr{Type: design.String}, ElemType: &design.AttributeExpr{Type: design.String}}
+	if tk == nil {
+		eval.ReportError("invalid MapOf key argument: not a type and not a known user type name")
+		return res
+	}
+	if tv == nil {
+		eval.ReportError("invalid MapOf value argument: not a type and not a known user type name")
+		return res
+	}
+	if len(dsl) > 1 {
+		eval.ReportError("MapOf: too many arguments")
+		return res
+	}
+	kat := design.AttributeExpr{Type: tk}
+	vat := design.AttributeExpr{Type: tv}
+	m := &design.Map{KeyType: &kat, ElemType: &vat}
+	if len(dsl) == 1 {
+		mat := design.AttributeExpr{Type: m}
+		eval.Execute(dsl[0], &mat)
+	}
+	return m
+}
+
+// Key makes it possible to specify validations for map keys.
+func Key(dsl func()) {
+	at, ok := eval.Current().(*design.AttributeExpr)
+	if !ok {
+		eval.IncompatibleDSL()
+		return
+	}
+	m := at.Type.(*design.Map)
+	eval.Execute(dsl, m.KeyType)
+}
+
+// Value makes it possible to specify validations for map values.
+func Value(dsl func()) {
+	at, ok := eval.Current().(*design.AttributeExpr)
+	if !ok {
+		eval.IncompatibleDSL()
+		return
+	}
+	m := at.Type.(*design.Map)
+	eval.Execute(dsl, m.ElemType)
 }
