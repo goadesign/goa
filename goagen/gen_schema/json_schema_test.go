70b24d918d562974090308d9303121a287892625
azihsoyn <azihsoyn@gmail.com>
2016-11-22 02:57:06 +0900

    fix import

--- goagen/gen_schema/json_schema_test.go
+++ goagen/gen_schema/json_schema_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/goadesign/goa/design"
 	. "github.com/goadesign/goa/design/apidsl"
 	"github.com/goadesign/goa/dslengine"
+	"github.com/goadesign/goa/goagen/gen_schema"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
