fabc68c8b1437d7753e700388427512ccdd851b0
Raphael Simon <simon.raphael@gmail.com>
2017-01-09 22:04:39 -0800

    Add Tracer and AWS X-Ray middlewares.
    The Tracer middleware retrieves the trace ID and parent span ID from the
    incoming request if present and stores the in the context. The middleware
    provides a goa Doer wrapper function that extract the values from the context
    and sets them in the outgoing request.
    
    The middleware constructor accepts a percentage as argument which specifies the
    sampling value. This value is used when the incoming request does *not* include
    tracing headers to determine whether a new trace ID should be computed and set
    in the context.
    
    The xray middleware leverages the values set in the context by the Tracer
    middleware to report tracing information to the AWS X-Ray service. The package
    also makes it possible to attach further information to the traces such as
    annotations and metadata. The middleware also reports errors to the service.
    
    The xray package also provides a WrapClient function for wrapping an instance
    of http.Client to trace requests made to external services.

---
+++ middleware/tracer_test.go
@@ -0,0 +1,104 @@
+package middleware
+
+import (
+	"math"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"golang.org/x/net/context"
+)
+
+func TestNew(t *testing.T) {
+	cases := map[string]struct{ Rate int }{
+		"zero":  {0},
+		"one":   {1},
+		"fifty": {50},
+		"100":   {100},
+	}
+
+	for k, c := range cases {
+		m := Tracer(c.Rate, nil, nil)
+		if m == nil {
+			t.Errorf("%s: Tracer return nil", k)
+		}
+	}
+
+	cases = map[string]struct{ Rate int }{
+		"negative":  {-1},
+		"one-o-one": {101},
+		"maxint":    {math.MaxInt64},
+	}
+
+	for k, c := range cases {
+		func() {
+			defer func() {
+				r := recover()
+				if r != "tracing: sample rate must be between 0 and 100" {
+					t.Errorf("%s: Tracer did *not* panic", k)
+				}
+			}()
+			Tracer(c.Rate, nil, nil)
+		}()
+	}
+}
+
+func TestMiddleware(t *testing.T) {
+	var (
+		traceID    = "testTraceID"
+		spanID     = "testSpanID"
+		newTraceID = func() string { return traceID }
+		newID      = func() string { return spanID }
+	)
+
+	cases := map[string]struct {
+		Rate                  int
+		TraceID, ParentSpanID string
+		// output
+		CtxTraceID, CtxSpanID, CtxParentID string
+	}{
+		"no-trace": {100, "", "", traceID, spanID, ""},
+		"trace":    {100, "trace", "", "trace", spanID, ""},
+		"parent":   {100, "trace", "parent", "trace", spanID, "parent"},
+
+		"zero-rate-no-trace": {0, "", "", "", "", ""},
+		"zero-rate-trace":    {0, "trace", "", "trace", spanID, ""},
+		"zero-rate-parent":   {0, "trace", "parent", "trace", spanID, "parent"},
+	}
+
+	for k, c := range cases {
+		var (
+			ctxTraceID, ctxSpanID, ctxParentID string
+
+			m = Tracer(c.Rate, newID, newTraceID)
+			h = func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
+				ctxTraceID = ContextTraceID(ctx)
+				ctxSpanID = ContextSpanID(ctx)
+				ctxParentID = ContextParentSpanID(ctx)
+				return nil
+			}
+			headers = make(http.Header)
+			ctx     = context.Background()
+		)
+		if c.TraceID != "" {
+			headers.Set(TraceIDHeader, c.TraceID)
+		}
+		if c.ParentSpanID != "" {
+			headers.Set(ParentSpanIDHeader, c.ParentSpanID)
+		}
+		req, _ := http.NewRequest("GET", "/", nil)
+		req.Header = headers
+
+		m(h)(ctx, httptest.NewRecorder(), req)
+
+		if ctxTraceID != c.CtxTraceID {
+			t.Errorf("%s: invalid TraceID, expected %v - got %v", k, c.CtxTraceID, ctxTraceID)
+		}
+		if ctxSpanID != c.CtxSpanID {
+			t.Errorf("%s: invalid SpanID, expected %v - got %v", k, c.CtxSpanID, ctxSpanID)
+		}
+		if ctxParentID != c.CtxParentID {
+			t.Errorf("%s: invalid ParentSpanID, expected %v - got %v", k, c.CtxParentID, ctxParentID)
+		}
+	}
+}
---
+++ middleware/xray/client.go
@@ -0,0 +1,56 @@
+package xray
+
+import (
+	"net/http"
+
+	"golang.org/x/net/context"
+)
+
+type (
+	// Doer is the http Client interface.
+	Doer interface {
+		Do(*http.Request) (*http.Response, error)
+	}
+
+	// httpTracer is a http client that creates subsegments for each request
+	// it makes.
+	httpTracer struct {
+		client  *http.Client
+		segment *Segment
+	}
+)
+
+// WrapClient wraps a http client and creates subsegments of the segment in the
+// context for each request it makes. The subsegments created this way have
+// their namespace set to "remote".
+//
+// ctx must contain the current request segment as set by the xray middleware or
+// the client passed as argument is returned.
+func WrapClient(ctx context.Context, c *http.Client) Doer {
+	s := ContextSegment(ctx)
+	if s == nil {
+		return c
+	}
+	return &httpTracer{
+		client:  c,
+		segment: s,
+	}
+}
+
+// Do reates a subsegment and makes the request.
+func (r *httpTracer) Do(req *http.Request) (*http.Response, error) {
+	sub := r.segment.NewSubsegment(req.URL.Host)
+	defer sub.Close()
+	sub.Namespace = "remote"
+	sub.HTTP = &HTTP{Request: requestData(req)}
+
+	resp, err := r.client.Do(req)
+
+	if err != nil {
+		sub.recordError(err, resp.StatusCode < 500)
+		return nil, err
+	}
+	sub.HTTP.Response = responseData(resp)
+
+	return resp, err
+}
