76bb6552e87664548fc0f78f6d11dfc20bdd64ca
Raphael Simon <simon.raphael@gmail.com>
2017-01-09 22:04:39 -0800

    Add Tracer and AWS X-Ray middlewares.
    The Tracer middleware retrieves the trace ID and parent span ID from the
    incoming request if present and stores the in the context. The middleware
    provides a goa Doer wrapper function that extract the values from the context
    and sets them in the outgoing request.
    
    The middleware constructor accepts a percentage as argument which specifies the
    sampling value. This value is used when the incoming request does *not* include
    tracing headers to determine whether a new trace ID should be computed and set
    in the context.
    
    The xray middleware leverages the values set in the context by the Tracer
    middleware to report tracing information to the AWS X-Ray service. The package
    also makes it possible to attach further information to the traces such as
    annotations and metadata. The middleware also reports errors to the service.
    
    The xray package also provides a WrapClient function for wrapping an instance
    of http.Client to trace requests made to external services.

---
+++ middleware/tracer.go
@@ -0,0 +1,143 @@
+package middleware
+
+import (
+	rd "math/rand"
+	"net/http"
+
+	"github.com/goadesign/goa"
+	"github.com/goadesign/goa/client"
+	"golang.org/x/net/context"
+)
+
+var (
+	// TraceIDHeader is the name of the HTTP request header containing the
+	// current TraceID if any.
+	TraceIDHeader = "TraceID"
+
+	// ParentSpanIDHeader is the name of the HTTP request header containing
+	// the parent span ID if any.
+	ParentSpanIDHeader = "ParentSpanID"
+)
+
+type (
+	// IDFunc is a function that produces span and trace IDs for cosumption by
+	// tracing systems such as Zipkin or AWS X-Ray.
+	IDFunc func() string
+
+	// tracedDoer is a goa client Doer that inserts the tracing headers for
+	// each request it makes.
+	tracedDoer struct {
+		doer    client.Doer
+		traceID string
+		spanID  string
+	}
+)
+
+// Tracer returns a middleware that initializes the trace information in the
+// context. The information can be retrieved using any of the ContextXXX
+// functions.
+//
+// sampleRate must be a value between 0 and 100. It represents the percentage of
+// requests that should be traced.
+//
+// spanIDFunc and traceIDFunc are the functions used to create Span and Trace
+// IDs respectively. This is configurable so that the created IDs are compatible
+// with the various backend tracing systems. The xray package provides
+// implementations that produce AWS X-Ray compatible IDs.
+//
+// If the incoming request has a TraceIDHeader header then the sample rate is
+// disregarded and the tracing is enabled.
+func Tracer(sampleRate int, spanIDFunc, traceIDFunc IDFunc) goa.Middleware {
+	if sampleRate < 0 || sampleRate > 100 {
+		panic("tracing: sample rate must be between 0 and 100")
+	}
+	return func(h goa.Handler) goa.Handler {
+		return func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
+			// Compute trace info.
+			var (
+				traceID  = req.Header.Get(TraceIDHeader)
+				parentID = req.Header.Get(ParentSpanIDHeader)
+				spanID   = spanIDFunc()
+			)
+			if traceID == "" {
+				// Avoid computing a random value if unnecessary.
+				if sampleRate == 0 || rd.Intn(100) > sampleRate {
+					return h(ctx, rw, req)
+				}
+				traceID = traceIDFunc()
+			}
+
+			// Setup context.
+			ctx = WithTrace(ctx, traceID, spanID, parentID)
+
+			// Call next handler.
+			return h(ctx, rw, req)
+		}
+	}
+}
+
+// TraceDoer wraps a goa client Doer and sets the trace headers so that the
+// downstream service may properly retrieve the parent span ID and trace ID.
+//
+// ctx must contain the current request segment as set by the xray middleware or
+// the doer passed as argument is returned.
+func TraceDoer(ctx context.Context, doer client.Doer) client.Doer {
+	var (
+		traceID = ContextTraceID(ctx)
+		spanID  = ContextSpanID(ctx)
+	)
+	if traceID == "" {
+		return doer
+	}
+	return &tracedDoer{
+		doer:    doer,
+		traceID: traceID,
+		spanID:  spanID,
+	}
+}
+
+// ContextTraceID returns the trace ID extracted from the given context if any,
+// the empty string otherwise.
+func ContextTraceID(ctx context.Context) string {
+	if t := ctx.Value(traceKey); t != nil {
+		return t.(string)
+	}
+	return ""
+}
+
+// ContextSpanID returns the span ID extracted from the given context if any,
+// the empty string otherwise.
+func ContextSpanID(ctx context.Context) string {
+	if s := ctx.Value(spanKey); s != nil {
+		return s.(string)
+	}
+	return ""
+}
+
+// ContextParentSpanID returns the parent span ID extracted from the given
+// context if any, the empty string otherwise.
+func ContextParentSpanID(ctx context.Context) string {
+	if p := ctx.Value(parentSpanKey); p != nil {
+		return p.(string)
+	}
+	return ""
+}
+
+// WithTrace returns a context containing the given trace, span and parent span
+// IDs.
+func WithTrace(ctx context.Context, traceID, spanID, parentID string) context.Context {
+	if parentID != "" {
+		ctx = context.WithValue(ctx, parentSpanKey, parentID)
+	}
+	ctx = context.WithValue(ctx, traceKey, traceID)
+	ctx = context.WithValue(ctx, spanKey, spanID)
+	return ctx
+}
+
+// Do adds the tracing headers to the requests before making it.
+func (d *tracedDoer) Do(ctx context.Context, req *http.Request) (*http.Response, error) {
+	req.Header.Set(TraceIDHeader, d.traceID)
+	req.Header.Set(ParentSpanIDHeader, d.spanID)
+
+	return d.doer.Do(ctx, req)
+}
---
+++ middleware/xray/segment_test.go
@@ -0,0 +1,76 @@
+package xray
+
+import (
+	"regexp"
+	"sync"
+	"testing"
+
+	"github.com/pkg/errors"
+)
+
+func TestRecordError(t *testing.T) {
+	var (
+		errMsg       = "foo"
+		cause        = "cause"
+		inner        = "inner"
+		err          = errors.New(errMsg)
+		wrapped      = errors.Wrap(err, cause)
+		wrappedTwice = errors.Wrap(wrapped, inner)
+	)
+	cases := map[string]struct {
+		Error    error
+		Message  string
+		HasCause bool
+	}{
+		"go-error":     {err, errMsg, false},
+		"wrapped":      {wrapped, cause + ": " + errMsg, true},
+		"wrappedTwice": {wrappedTwice, inner + ": " + cause + ": " + errMsg, true},
+	}
+	for k, c := range cases {
+		s := Segment{}
+		s.recordError(c.Error, false)
+		w := s.Cause.Exceptions[0]
+		if w.Message != c.Message {
+			t.Errorf("%s: invalid message, expected %s got %s", k, c.Message, w.Message)
+		}
+		if c.HasCause && len(w.Stack) < 2 {
+			t.Errorf("%s: stack too small: %v", k, w.Stack)
+		}
+	}
+}
+
+func TestNewSubsegment(t *testing.T) {
+	var (
+		name   = "sub"
+		s      = &Segment{Mutex: &sync.Mutex{}}
+		before = now()
+		ss     = s.NewSubsegment(name)
+	)
+	if s.counter != 1 {
+		t.Errorf("counter not incremented after call to Subsegment")
+	}
+	if len(s.Subsegments) != 1 {
+		t.Fatalf("invalid count of subsegments, expected 1 got %d", len(s.Subsegments))
+	}
+	if s.Subsegments[0] != ss {
+		t.Errorf("invalid subsegments element, expected %v - got %v", name, s.Subsegments[0])
+	}
+	if ss.ID == "" {
+		t.Errorf("subsegment ID not initialized")
+	}
+	if !regexp.MustCompile("[0-9a-f]{16}").MatchString(ss.ID) {
+		t.Errorf("invalid subsegment ID, got %v", ss.ID)
+	}
+	if ss.Name != name {
+		t.Errorf("invalid subsegemnt name, expected %s got %s", name, ss.Name)
+	}
+	if ss.StartTime < before {
+		t.Errorf("invalid subsegment StartAt, expected at least %v, got %v", before, ss.StartTime)
+	}
+	if !ss.InProgress {
+		t.Errorf("subsegemnt not in progress")
+	}
+	if ss.Parent != s {
+		t.Errorf("invalid subsegment parent, expected %v, got %v", s, ss.Parent)
+	}
+}
