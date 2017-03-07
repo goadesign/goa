8e8cb89c5d726dbe8c5b06d17cc325e3c711d541
Raphael Simon <simon.raphael@gmail.com>
2017-03-07 10:10:06 -0800

    Update go-kit log adapter to support latest

--- logging/kit/adapter_test.go
+++ logging/kit/adapter_test.go
@@ -3,8 +3,6 @@ package goakit_test
 import (
 	"bytes"
 
-	"golang.org/x/net/context"
-
 	"github.com/go-kit/kit/log"
 	"github.com/goadesign/goa"
 	"github.com/goadesign/goa/logging/kit"
@@ -26,35 +24,5 @@ var _ = Describe("New", func() {
 		msg := "msg"
 		adapter.Info(msg)
 		Ω(buf.String()).Should(Equal("lvl=info msg=" + msg + "\n"))
-	})
-})
-
-var _ = Describe("FromContext", func() {
-	var buf bytes.Buffer
-	var logctx *log.Context
-	var adapter goa.LogAdapter
-
-	BeforeEach(func() {
-		logger := log.NewLogfmtLogger(&buf)
-		logctx = log.NewContext(logger)
-		adapter = goakit.FromContext(logctx)
-	})
-
-	It("creates an adapter that logs", func() {
-		msg := "msg"
-		adapter.Info(msg)
-		Ω(buf.String()).Should(Equal("lvl=info msg=" + msg + "\n"))
-	})
-
-	Context("Context", func() {
-		var ctx context.Context
-
-		BeforeEach(func() {
-			ctx = goa.WithLogger(context.Background(), adapter)
-		})
-
-		It("extracts the log context", func() {
-			Ω(goakit.Context(ctx)).Should(Equal(logctx))
-		})
 	})
 })
