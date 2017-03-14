da1c0d51404d34623afa836201297fd055afd38c
Raphaël Simon <raphael@rightscale.com>
2017-03-13 20:09:31 -0700

    Initial work on goagen
    * Initial stab at goagen main package.
    
    More work to do.
    
    * Do not run lint in appveyor
    
    * Continue implementation of goagen tool
    
    * Make generated goagen compile and run.

--- cmd/goagen/main.go
+++ cmd/goagen/main.go
@@ -2,59 +2,160 @@ package main
 
 import (
 	"fmt"
+	"go/build"
 	"os"
-
-	"flag"
+	"strings"
 
 	"goa.design/goa.v2/pkg"
 
-	// These are packages required by the generated code but not by goagen.
-	// We list them here so that `go get` picks them up.
-	_ "gopkg.in/yaml.v2"
+	"flag"
 )
 
 func main() {
 	var (
-		// out     = flag.String("out", ".", "Output `directory`")
-		// design  = flag.String("design", "", "Design Go import `package path`")
-		// debug   = flag.Bool("debug", false, "Print debug information")
-		version = flag.Bool("version", false, "Print version information")
+		cmds []string
+		path string
 	)
+	{
+		if len(os.Args) == 1 {
+			usage()
+		}
 
-	flag.Usage = usage
-	flag.Parse()
-
-	if *version {
-		fmt.Println("goagen " + pkg.Version() + "\nThe goa generation tool.")
-		os.Exit(0)
+		switch os.Args[1] {
+		case "version":
+			fmt.Println("goagen version " + pkg.Version())
+			os.Exit(0)
+		case "client", "server", "openapi":
+			if len(os.Args) == 2 {
+				usage()
+			}
+			cmds = []string{os.Args[1]}
+			i := 2
+			for len(os.Args) > i+1 &&
+				(os.Args[i] == "client" ||
+					os.Args[i] == "server" ||
+					os.Args[i] == "openapi") {
+				cmds = append(cmds, os.Args[i])
+				i++
+			}
+			path = os.Args[i]
+		default:
+			usage()
+		}
 	}
 
-	if len(os.Args) < flag.NFlag()+2 {
-		fmt.Fprint(os.Stderr, "Missing command\n")
-		os.Exit(1)
+	var (
+		output      string
+		gens, debug bool
+	)
+	if len(os.Args) > 3 {
+		var (
+			fset     = flag.NewFlagSet("default", flag.ExitOnError)
+			o        = fset.String("o", "", "output `directory`")
+			out      = fset.String("out", ".", "output `directory`")
+			s        = fset.Bool("s", false, "Generate scaffold (does not override existing files)")
+			scaffold = fset.Bool("scaffold", false, "Generate scaffold (does not override existing files)")
+		)
+		fset.BoolVar(&debug, "debug", false, "Print debug information")
+
+		fset.Usage = usage
+		fset.Parse(os.Args[3:])
+
+		output = *o
+		if output == "" {
+			output = *out
+		}
+
+		gens = *s
+		if !gens {
+			gens = *scaffold
+		}
 	}
 
-	// cmd := command(os.Args[flag.NFlag()+1])
-	// command.Run(*out, *design, *debug)
+	if _, err := build.Import(path, ".", build.IgnoreVendor); err != nil {
+		fail(err)
+	}
+
+	var (
+		tmp *GenPackage
+	)
+	{
+		tmp = NewGenPackage(cmds, path, output)
+		defer tmp.Remove()
+	}
+
+	if err := tmp.Write(gens, debug); err != nil {
+		fail(err)
+	}
+
+	if err := tmp.Compile(); err != nil {
+		fail(err)
+	}
+
+	files, err := tmp.Run()
+	if err != nil {
+		fail(err)
+	}
+
+	fmt.Println(strings.Join(files, "\n"))
+}
+
+func fail(err error) {
+	fmt.Fprint(os.Stderr, err.Error())
+	os.Exit(1)
 }
 
 func usage() {
-	fmt.Fprint(os.Stderr, `The goagen tool generates artifacts from a goa design package.
+	fmt.Fprint(os.Stderr, `goagen is the goa code generation tool.
+Learn more about goa at https://goa.design.
 
-Each command supported by the tool produces a specific type of artifacts. For
-example the "server" command generates the code that supports the service
-server.
+The tool supports multiple subcommands that generate different outputs.
+The only argument is the Go import path to the service design package.
 
-The "scaffold" command is a special command that generates code only once.
-The generated code is intended to be consumed as if it had not been generated.
-That is the code should be edited, versioned, tested and in general maintained
-like any non generated code. It is generated as a convenience to help get
-started.
+The "--scaffold" flag tells goagen to also generate the scaffold for the service
+and/or the client depending on which command is being executed. The scaffold is
+code that is generated once as a way to get started quickly. It should be edited
+after the initial generation.
 
-The "bootstrap" command runs the "scaffold", "server", "client" and "openapi"
-commands generating all the artifacts needed to start implementing a new service.
+Usage:
 
-Usage of goagen:
+  goagen [server] [client] [openapi] PACKAGE [--out DIRECTORY] [--scaffold] [--debug]
+
+  goagen version
+
+Commands:
+  server
+        Generate service interfaces, endpoints and server transport code.
+
+  client
+        Generate endpoints and client transport code.
+
+  openapi
+        Generate OpenAPI specification (https://www.openapis.org/).
+
+  version
+        Print version information (exclusive with other flags and commands).
+
+Args:
+  PACKAGE
+        Go import path to design package
+
+Flags:
+  -o, --out DIRECTORY
+        output directory, defaults to the current working directory
+
+  -s, --scaffold
+        generate scaffold (does not override existing files)
+
+  --debug
+        Print debug information (mainly intended for goa developers)
+
+Examples:
+
+  goagen server goa.design/cellar/design
+
+  goagen server client openapi goa.design/cellar/design -o gen -s
+
 `)
-	flag.PrintDefaults()
+	os.Exit(1)
 }
