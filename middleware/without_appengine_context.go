6d95c6cb52595fdccae53b08bd7060ff9b20f2f9
Daniel Hoffmann <oralordos@gmail.com>
2017-07-24 16:52:15 -0700

    Add appengine context middleware

---
+++ middleware/without_appengine_context.go
@@ -0,0 +1,18 @@
+// +build !appengine
+
+package middleware
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/goadesign/goa"
+)
+
+// AppEngineContext is a middleware that ensures that the goa contexts are valid App Engine contexts
+// It is safe to use in a non-appengine environment
+func AppEngineContext(h goa.Handler) goa.Handler {
+	return func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
+		return h(ctx, rw, req)
+	}
+}
