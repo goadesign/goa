19eeae3c6fb1eff2798d3312e03ba6c34818127d
Daniel <oralordos@gmail.com>
2017-07-26 16:05:41 -0700

    Simplify non-appengine code

--- middleware/without_appengine_context.go
+++ middleware/without_appengine_context.go
@@ -12,7 +12,5 @@ import (
 // AppEngineContext is a middleware that ensures that the goa contexts are valid App Engine contexts
 // It is safe to use in a non-appengine environment
 func AppEngineContext(h goa.Handler) goa.Handler {
-	return func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
-		return h(ctx, rw, req)
-	}
+	return h
 }
