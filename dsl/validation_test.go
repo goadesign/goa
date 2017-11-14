8dffa461e62d83718953c8b802dcd18415992cfa
Taichi Sasaki <tchsskk@gmail.com>
2017-11-14 11:59:02 +0900

    Add tests for the DSL Format

---
+++ dsl/validation_test.go
@@ -0,0 +1,42 @@
+package dsl_test
+
+import (
+	"testing"
+
+	"goa.design/goa/design"
+	. "goa.design/goa/dsl"
+	"goa.design/goa/eval"
+)
+
+func TestFormat(t *testing.T) {
+	cases := map[string]struct {
+		Format design.ValidationFormat
+	}{
+		"date-time": {design.FormatDateTime},
+		"email":     {design.FormatEmail},
+		"hostname":  {design.FormatHostname},
+		"ipv4":      {design.FormatIPv4},
+		"ipv6":      {design.FormatIPv6},
+		"ip":        {design.FormatIP},
+		"uri":       {design.FormatURI},
+		"mac":       {design.FormatMAC},
+		"cidr":      {design.FormatCIDR},
+		"regexp":    {design.FormatRegexp},
+	}
+
+	for k, tc := range cases {
+		eval.Context = &eval.DSLContext{}
+		expr := &design.AttributeExpr{}
+		eval.Execute(func() { Format(tc.Format) }, expr)
+		if eval.Context.Errors != nil {
+			t.Errorf("%s: Format failed unexpectedly with %s", k, eval.Context.Errors)
+		}
+		if expr.Validation == nil {
+			t.Errorf("%s: Format not initialized Validation in %+v", k, expr)
+		} else {
+			if expr.Validation.Format != tc.Format {
+				t.Errorf("%s: Format not set on %+v, expected %s, got %+v", k, expr, tc.Format, expr.Validation.Format)
+			}
+		}
+	}
+}
