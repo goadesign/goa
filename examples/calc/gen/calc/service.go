9dcbf95c115a79c443e1092b7be7e607dc7a38e1
Raphaël Simon <simon.raphael@gmail.com>
2018-03-17 16:56:12 -0700

    Add service and method names in generated service source as constants (#1607)

--- examples/calc/gen/calc/service.go
+++ examples/calc/gen/calc/service.go
@@ -18,6 +18,16 @@ type Service interface {
 	Add(context.Context, *AddPayload) (int, error)
 }
 
+// ServiceName is the name of the service as defined in the design. This is the
+// same value that is set in the endpoint request contexts under the ServiceKey
+// key.
+const ServiceName = "calc"
+
+// MethodNames lists the service method names as defined in the design. These
+// are the same values that are set in the endpoint request contexts under the
+// MethodKey key.
+var MethodNames = []string{"add"}
+
 // AddPayload is the payload type of the calc service add method.
 type AddPayload struct {
 	// Left operand
--- examples/calc/gen/openapi/service.go
+++ examples/calc/gen/openapi/service.go
@@ -11,3 +11,13 @@ package openapi
 // Service is the openapi service interface.
 type Service interface {
 }
+
+// ServiceName is the name of the service as defined in the design. This is the
+// same value that is set in the endpoint request contexts under the ServiceKey
+// key.
+const ServiceName = "openapi"
+
+// MethodNames lists the service method names as defined in the design. These
+// are the same values that are set in the endpoint request contexts under the
+// MethodKey key.
+var MethodNames = []string{}
