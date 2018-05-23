b30a7c24c69f27d0f8eb4d0aa12b15d83df6d908
Scott Messier <scott@rightscale.com>
2018-05-23 11:32:15 -0700

    use_x_request_id (#1723)
    * added options for RequestID middleware
    same default behavior but with options to use incoming "X-Request-Id" header
    added unit tests
    
    * fixed CR issues
    added examples to RequestID function documentation
    made middleware slightly more efficient
    added missing test case

---
+++ http/middleware/requestid_test.go
@@ -0,0 +1,101 @@
+package middleware_test
+
+import (
+	"net/http"
+	"testing"
+
+	"goa.design/goa/http/middleware"
+)
+
+type (
+	requestIDTestHandler struct {
+		testCaseName string
+		handler http.HandlerFunc
+	}
+
+	requestIDTestCase struct {
+		name    string
+		options []middleware.RequestIDOption
+		request *http.Request
+		handler http.HandlerFunc
+	}
+)
+
+func TestRequestID(t *testing.T) {
+	var (
+		getRequestID = func(r *http.Request) (id string) {
+			id, _ = r.Context().Value(middleware.RequestIDKey).(string)
+			return
+		}
+		makeRequest = func(xRequestIDValue string) (r *http.Request) {
+			r = &http.Request{
+				Header: make(http.Header),
+			}
+			if len(xRequestIDValue) > 0 {
+				r.Header.Set("X-Request-Id", xRequestIDValue)
+			}
+			return
+		}
+		ignored http.ResponseWriter
+	)
+	for _, tc := range []*requestIDTestCase{
+		&requestIDTestCase{"default without header", nil, makeRequest(""),
+			func(_ http.ResponseWriter, r *http.Request) {
+				id := getRequestID(r)
+				if len(id) != 8 {
+					t.Errorf("%s: unexpected request ID length: %d != 8", r.Header.Get("Test-Case"), len(id))
+				}
+			},
+		},
+		&requestIDTestCase{"default ignores header", nil, makeRequest("ignore this header"),
+			func(_ http.ResponseWriter, r *http.Request) {
+				id := getRequestID(r)
+				if len(id) != 8 {
+					t.Errorf("%s: unexpected request ID length: %d != 8", r.Header.Get("Test-Case"), len(id))
+				}
+			},
+		},
+		&requestIDTestCase{"generate without header",
+			[]middleware.RequestIDOption{middleware.UseXRequestIDHeaderOption(true)},
+			makeRequest(""),
+			func(_ http.ResponseWriter, r *http.Request) {
+				id := getRequestID(r)
+				if len(id) != 8 {
+					t.Errorf("%s: unexpected request ID length: %d != 8", r.Header.Get("Test-Case"), len(id))
+				}
+			},
+		},
+		&requestIDTestCase{"accept header",
+			[]middleware.RequestIDOption{middleware.UseXRequestIDHeaderOption(true)},
+			makeRequest("accept+header"),
+			func(_ http.ResponseWriter, r *http.Request) {
+				id := getRequestID(r)
+				if id != "accept+header" {
+					t.Errorf("%s: unexpected request ID length: %s != accept+header", r.Header.Get("Test-Case"), id)
+				}
+			},
+		},
+		&requestIDTestCase{"truncate header",
+			[]middleware.RequestIDOption{
+				middleware.UseXRequestIDHeaderOption(true),
+				middleware.XRequestHeaderLimitOption(3),
+			},
+			makeRequest("too long for length limit"),
+			func(_ http.ResponseWriter, r *http.Request) {
+				id := getRequestID(r)
+				if id != "too" {
+					t.Errorf("%s: unexpected request ID length: %s != too", r.Header.Get("Test-Case"), id)
+				}
+			},
+		},
+	} {
+		middleware.RequestID(tc.options...)(
+			&requestIDTestHandler{tc.name, tc.handler}).ServeHTTP(ignored, tc.request)
+	}
+}
+
+// ServeHTTP implements http.Handler#ServeHTTP
+func (h *requestIDTestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	r.Header.Set("Test-Case", h.testCaseName)
+	h.handler(w, r)
+}
