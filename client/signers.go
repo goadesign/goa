8c6866ed6391670571d58220001457125f0dd96f
David Plumpton <david.plumpton@gmail.com>
2018-06-15 12:05:44 +1200

    Work in progress adding websocket signers

--- client/signers.go
+++ client/signers.go
@@ -8,16 +8,22 @@ import (
 	"golang.org/x/net/websocket"
 )
 
+const (
+	// AuthHeader is the standard Authorization header name.
+	AuthHeader = "Authorization"
+)
+
 type (
-	// Signer is the common interface implemented by all signers.
+	// Signer is the common interface implemented by all http signers.
 	Signer interface {
 		// Sign adds required headers, cookies etc.
 		Sign(*http.Request) error
 	}
 
-	// WebSocketSigner is the interface implemented by all signers using websocket.
-	WebSocketSigner interface {
-		SignWebSocket(*websocket.Conn) error
+	// WsSigner is the common interface for websocket signers
+	WsSigner interface {
+		// WsSign adds required headers, cookies, etc. to websocket config
+		WsSign(*websocket.Conn) error
 	}
 
 	// BasicSigner implements basic auth.
@@ -63,6 +69,8 @@ type (
 	Token interface {
 		// SetAuthHeader sets the Authorization header to r.
 		SetAuthHeader(r *http.Request)
+		// SetWsAuthHeader sets the Authorization header to cfg.
+		SetWsAuthHeader(cfg *websocket.Config)
 		// Valid reports whether Token can be used to properly sign requests.
 		Valid() bool
 	}
@@ -89,7 +97,7 @@ type (
 	}
 )
 
-// Sign adds the basic auth header to the request.
+// Sign adds the basic auth header to the http request.
 func (s *BasicSigner) Sign(req *http.Request) error {
 	if s.Username != "" && s.Password != "" {
 		req.SetBasicAuth(s.Username, s.Password)
@@ -97,18 +105,18 @@ func (s *BasicSigner) Sign(req *http.Request) error {
 	return nil
 }
 
-// SignWebSocket adds the basic auth header to the WebSocket dial config
-func (s *WebSocketSigner) SignWebSocket(cfg *websocket.Config) error {
+// WsSign adds the basic auth header to the websocket config
+func (s *BasicSigner) WsSign(cfg *websocket.Config) error {
 	if s.Username != "" && s.Password != "" {
-		cfg.Header.Set("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(s.Username+":"+s.Password)))
+		cfg.Header.Set(AuthHeader, "Basic "+base64.StdEncoding.EncodeToString([]byte(s.Username+":"+s.Password)))
 	}
 	return nil
 }
 
-// Sign adds the API key header to the request.
+// Sign adds the API key header to the http request.
 func (s *APIKeySigner) Sign(req *http.Request) error {
 	if s.KeyName == "" {
-		s.KeyName = "Authorization"
+		s.KeyName = AuthHeader
 	}
 	if s.Format == "" {
 		s.Format = "Bearer %s"
@@ -126,17 +134,42 @@ func (s *APIKeySigner) Sign(req *http.Request) error {
 	return nil
 }
 
-// Sign adds the JWT auth header.
+// WsSign adds the API key header to the websocket config
+func (s *APIKeySigner) WsSign(cfg *websocket.Config) error {
+	if s.KeyName == "" {
+		s.KeyName = AuthHeader
+	}
+	if s.Format == "" {
+		s.Format = "Bearer %s"
+	}
+	name := s.KeyName
+	format := s.Format
+	val := fmt.Sprintf(format, s.KeyValue)
+	cfg.Header.Set(name, val)
+	return nil
+}
+
+// Sign adds the JWT auth header to the http request.
 func (s *JWTSigner) Sign(req *http.Request) error {
 	return signFromSource(s.TokenSource, req)
 }
 
-// Sign refreshes the access token if needed and adds the OAuth header.
+// WsSign adds the JWT auth header to the websocket config.
+func (s *JWTSigner) WsSign(cfg *websocket.Config) error {
+	return signWsFromSource(s.TokenSource, cfg)
+}
+
+// Sign refreshes the access token if needed and adds the OAuth header to the http request.
 func (s *OAuth2Signer) Sign(req *http.Request) error {
 	return signFromSource(s.TokenSource, req)
 }
 
-// signFromSource generates a token using the given source and uses it to sign the request.
+// WsSign refreshes the access token if needed and adds the OAuth header to the websocket config.
+func (s *OAuth2Signer) WsSign(cfg *websocket.Config) error {
+	return signWsFromSource(s.TokenSource, cfg)
+}
+
+// signFromSource generates a token using the given source and uses it to sign the http request.
 func signFromSource(source TokenSource, req *http.Request) error {
 	token, err := source.Token()
 	if err != nil {
@@ -149,18 +182,40 @@ func signFromSource(source TokenSource, req *http.Request) error {
 	return nil
 }
 
+// signWsFromSource generates a token using the given source and uses it to create the websocket config.
+func signWsFromSource(source TokenSource, cfg *websocket.Config) error {
+	token, err := source.Token()
+	if err != nil {
+		return err
+	}
+	if !token.Valid() {
+		return fmt.Errorf("token expired or invalid")
+	}
+	token.SetWsAuthHeader(cfg)
+	return nil
+}
+
 // Token returns the static token.
 func (s *StaticTokenSource) Token() (Token, error) {
 	return s.StaticToken, nil
 }
 
-// SetAuthHeader sets the Authorization header to r.
+// SetAuthHeader sets the Authorization header to http request.
 func (t *StaticToken) SetAuthHeader(r *http.Request) {
 	typ := t.Type
 	if typ == "" {
 		typ = "Bearer"
 	}
-	r.Header.Set("Authorization", typ+" "+t.Value)
+	r.Header.Set(AuthHeader, typ+" "+t.Value)
+}
+
+// SetWsAuthHeader sets the Authorization header to websocket config.
+func (t *StaticToken) SetWsAuthHeader(c *websocket.Config) {
+	typ := t.Type
+	if typ == "" {
+		typ = "Bearer"
+	}
+	c.Header.Set(AuthHeader, typ+" "+t.Value)
 }
 
 // Valid reports whether Token can be used to properly sign requests.
