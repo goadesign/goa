f14c160845d974e9cf0a9a01abbca41b2adb9483
Raphaël Simon <simon.raphael@gmail.com>
2018-09-09 22:35:12 -0700

    Document CreateFrom and ConvertTo limitations (#1855)
    * docs
    
    * update aliases

--- http/dsl/aliases.go
+++ http/dsl/aliases.go
@@ -365,6 +365,12 @@ func Contact(fn func()) {
 // match is found or if the matching field type does not correspond to the
 // attribute type.
 //
+// The following limitations apply on the external Go struct field types
+// recursively:
+//
+//    * struct fields must use pointers
+//    * pointers on slices or on maps are not supported
+//
 // ConvertTo must appear in Type or ResutType.
 //
 // ConvertTo accepts one arguments: an instance of the external type.
@@ -425,6 +431,12 @@ func ConvertTo(obj interface{}) {
 // conversion code is generated for it. In all other cases it is an error if no
 // match is found or if the matching field type does not correspond to the
 // attribute type.
+//
+// The following limitations apply on the external Go struct field types
+// recursively:
+//
+//    * struct fields must use pointers
+//    * pointers on slices or on maps are not supported
 //
 // CreateFrom must appear in Type or ResutType.
 //
