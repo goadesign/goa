804fa17870b567cb979ff2ab60938e727dd2edf9
Taichi Sasaki <tchssk@users.noreply.github.com>
2019-01-15 02:51:19 +0900

    Add tests for expr.HostExpr.Validate() (#1958)
    * Remove punctuation from an error
    
    * Add tests for expr.HostExpr.Validate()

--- expr/server_test.go
+++ expr/server_test.go
@@ -1,8 +1,11 @@
 package expr
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
+
+	"goa.design/goa/eval"
 )
 
 func TestServerExprEvalName(t *testing.T) {
@@ -17,6 +20,158 @@ func TestServerExprEvalName(t *testing.T) {
 		server := ServerExpr{Name: tc.name}
 		if actual := server.EvalName(); actual != tc.expected {
 			t.Errorf("%s: got %#v, expected %#v", k, actual, tc.expected)
+		}
+	}
+}
+
+func TestHostExprValidate(t *testing.T) {
+	const (
+		foo = "foo"
+		bar = "bar"
+	)
+	var (
+		validURI         = URIExpr("http://example.com")
+		malformedURI     = URIExpr("http://%")
+		missingSchemeURI = URIExpr("example.com")
+		invalidSchemeURI = URIExpr("ftp:example.com")
+		validURIs        = []URIExpr{
+			validURI,
+		}
+		malformedURIs = []URIExpr{
+			malformedURI,
+		}
+		missingSchemeURIs = []URIExpr{
+			missingSchemeURI,
+		}
+		invalidSchemeURIs = []URIExpr{
+			invalidSchemeURI,
+		}
+		objectPrimitive = func(d interface{}, v *ValidationExpr) *Object {
+			return &Object{
+				{
+					Name: foo,
+					Attribute: &AttributeExpr{
+						Type:         Boolean,
+						DefaultValue: d,
+						Validation:   v,
+					},
+				},
+			}
+		}
+		objectNonPrimitive = func(d interface{}) *Object {
+			return &Object{
+				{
+					Name: bar,
+					Attribute: &AttributeExpr{
+						Type:         objectPrimitive(nil, nil),
+						DefaultValue: d,
+					},
+				},
+			}
+		}
+		attribute = func(d DataType) *AttributeExpr {
+			return &AttributeExpr{
+				Type: d,
+			}
+		}
+		errNoURI                          = fmt.Errorf("host must defined at least one URI")
+		errMalformedURI                   = fmt.Errorf("malformed URI %q", malformedURI)
+		errMissingSchemeURI               = fmt.Errorf("missing scheme for URI %q, scheme must be one of 'http', 'https', 'grpc' or 'grpcs'", missingSchemeURI)
+		errInvalidSchemeURI               = fmt.Errorf("invalid scheme for URI %q, scheme must be one of 'http', 'https', 'grpc' or 'grpcs'", invalidSchemeURI)
+		errInvalidType                    = fmt.Errorf("invalid type for URI variable %q: type must be a primitive", bar)
+		errNoDefaultValueOrEnumValidation = fmt.Errorf("URI variable %q must have a default value or an enum validation", foo)
+	)
+
+	cases := map[string]struct {
+		uris      []URIExpr
+		variables *AttributeExpr
+		expected  *eval.ValidationErrors
+	}{
+		"no error": {
+			uris: validURIs,
+			expected: &eval.ValidationErrors{
+				Errors: []error{},
+			},
+		},
+		"no uri": {
+			uris: []URIExpr{},
+			expected: &eval.ValidationErrors{
+				Errors: []error{
+					errNoURI,
+				},
+			},
+		},
+		"malformed uri": {
+			uris: malformedURIs,
+			expected: &eval.ValidationErrors{
+				Errors: []error{
+					errMalformedURI,
+				},
+			},
+		},
+		"missing scheme": {
+			uris: missingSchemeURIs,
+			expected: &eval.ValidationErrors{
+				Errors: []error{
+					errMissingSchemeURI,
+				},
+			},
+		},
+		"invalid scheme": {
+			uris: invalidSchemeURIs,
+			expected: &eval.ValidationErrors{
+				Errors: []error{
+					errInvalidSchemeURI,
+				},
+			},
+		},
+		"invalid type for uri variable": {
+			uris: validURIs,
+			variables: attribute(objectNonPrimitive(map[string]int{
+				"a": 1,
+			})),
+			expected: &eval.ValidationErrors{
+				Errors: []error{
+					errInvalidType,
+				},
+			},
+		},
+		"uri variable has no default value and no enum validation": {
+			uris:      validURIs,
+			variables: attribute(objectPrimitive(nil, nil)),
+			expected: &eval.ValidationErrors{
+				Errors: []error{
+					errNoDefaultValueOrEnumValidation,
+				},
+			},
+		},
+		"uri variable has no default value and no enum varidation values": {
+			uris: validURIs,
+			variables: attribute(objectPrimitive(nil, &ValidationExpr{
+				Values: []interface{}{},
+			})),
+			expected: &eval.ValidationErrors{
+				Errors: []error{
+					errNoDefaultValueOrEnumValidation,
+				},
+			},
+		},
+	}
+
+	for k, tc := range cases {
+		h := HostExpr{
+			URIs:      tc.uris,
+			Variables: tc.variables,
+		}
+		if actual := h.Validate().(*eval.ValidationErrors); len(tc.expected.Errors) != len(actual.Errors) {
+			t.Errorf("%s: expected the number of error values to match %d got %d ", k, len(tc.expected.Errors), len(actual.Errors))
+			t.Errorf(actual.Error())
+		} else {
+			for i, err := range actual.Errors {
+				if err.Error() != tc.expected.Errors[i].Error() {
+					t.Errorf("%s: got %#v, expected %#v at index %d", k, err, tc.expected.Errors[i], i)
+				}
+			}
 		}
 	}
 }
