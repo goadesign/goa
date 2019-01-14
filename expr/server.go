93954de5291c53503d99f7ecf306c695e1697435
Taichi Sasaki <tchssk@users.noreply.github.com>
2019-01-15 02:51:19 +0900

    Add tests for expr.HostExpr.Validate() (#1958)
    * Remove punctuation from an error
    
    * Add tests for expr.HostExpr.Validate()

--- expr/server.go
+++ expr/server.go
@@ -119,7 +119,7 @@ var validSchemes = map[string]struct{}{"http": {}, "https": {}, "grpc": {}, "grp
 func (h *HostExpr) Validate() error {
 	verr := new(eval.ValidationErrors)
 	if len(h.URIs) == 0 {
-		verr.Add(h, "host must defined at least one URI.")
+		verr.Add(h, "host must defined at least one URI")
 	}
 	for _, u := range h.URIs {
 		vu := WildcardRegex.ReplaceAllString(string(u), "/w")
