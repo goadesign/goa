ce5da1b7b9a4f1dcd074c59411612d69e259997e
Nitin Mohan <nitin@rightscale.com>
2019-01-30 13:50:24 -0800

    Add tests for grpc client example code

--- grpc/codegen/testdata/example_code.go
+++ grpc/codegen/testdata/example_code.go
@@ -170,3 +170,20 @@ func handleGRPCServer(ctx context.Context, u *url.URL, serviceEndpoints *service
 	}()
 }
 `
+
+const ExampleCLICode = `func doGRPC(scheme, host string, timeout int, debug bool) (goa.Endpoint, interface{}, error) {
+	conn, err := grpc.Dial(host, grpc.WithInsecure())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Sprintf("could not connect to gRPC server at %s: %v", host, err))
+	}
+	return cli.ParseEndpoint(conn)
+}
+
+func grpcUsageCommands() string {
+	return cli.UsageCommands()
+}
+
+func grpcUsageExamples() string {
+	return cli.UsageExamples()
+}
+`
