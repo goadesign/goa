ee6f2767bc2edefaedb4f05e14641b6e34d60e42
jim-slattery-rs <jim.slattery@rightscale.com>
2019-02-13 11:39:34 -0500

    IND-1925 save segment space by omitting redundant fields
    Several booleans fields are not required to be specified (false is implied),
    so we can save space in our traces by omitting them.
    
    Doing this by syncing up the Segment field definitions with the AWS SDK:
    https://github.com/aws/aws-xray-sdk-go/blob/852dc097d2b83915ece2ab4953e4de711d953626/xray/segment_model.go#L33-L45

--- middleware/xray/segment.go
+++ middleware/xray/segment.go
@@ -34,11 +34,11 @@ type (
 		// remote service. It is only initialized for the root segment.
 		ParentID string `json:"parent_id,omitempty"`
 		// StartTime is the segment start time.
-		StartTime float64 `json:"start_time,omitempty"`
+		StartTime float64 `json:"start_time"`
 		// EndTime is the segment end time.
 		EndTime float64 `json:"end_time,omitempty"`
 		// InProgress is true if the segment hasn't completed yet.
-		InProgress bool `json:"in_progress"`
+		InProgress bool `json:"in_progress,omitempty""`
 		// HTTP contains the HTTP request and response information and is
 		// only initialized for the root segment.
 		HTTP *HTTP `json:"http,omitempty"`
@@ -48,15 +48,15 @@ type (
 		// Error is true when a request causes an internal error. It is
 		// automatically set by Close when the response status code is
 		// 500 or more.
-		Error bool `json:"error"`
+		Error bool `json:"error,omitempty""`
 		// Fault is true when a request results in an error that is due
 		// to the user. Typically it should be set when the response
 		// status code is between 400 and 500 (but not 429).
-		Fault bool `json:"fault"`
+		Fault bool `json:"fault,omitempty""`
 		// Throttle is true when a request is throttled. It is set to
 		// true when the segment closes and the response status code is
 		// 429. Client code may set it to true manually as well.
-		Throttle bool `json:"throttle"`
+		Throttle bool `json:"throttle,omitempty""`
 		// Annotations contains the segment annotations.
 		Annotations map[string]interface{} `json:"annotations,omitempty"`
 		// Metadata contains the segment metadata.
