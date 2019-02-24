4a581331d5b714a54578f8dd39f45bb624551507
laches1sm <rebeccalmartin16@gmail.com>
2019-02-24 00:07:55 +0000

    Add tests for expr.DocsExpr.EvalName() and expr.ContactExpr.EvalName() (#2012)
    * Adding tests for EvalName for ContactExpr and DocsExpr
    
    * Final draft of tests
    
    * Adding suggested changes
    
    * Adding more test cases"

--- expr/api_test.go
+++ expr/api_test.go
@@ -130,6 +130,44 @@ func TestApiExpr_Hash(t *testing.T) {
 	}
 }
 
+func TestDocsExprEvalName(t *testing.T) {
+	cases := map[string]struct {
+		url         string
+		expected    string
+		description string
+	}{
+		"test, only with url":       {url: "http://parrot.com", expected: "Documentation http://parrot.com"},
+		"test, description and url": {url: "http://parrot.com", description: "A website for a parrot API", expected: "Documentation http://parrot.com"},
+		"test, only description":    {description: "A website for a parrot API", expected: "Documentation "},
+		"test, empty url":           {url: "", expected: "Documentation "},
+	}
+	for k, tc := range cases {
+		docExpr := DocsExpr{URL: tc.url, Description: tc.description}
+		if actual := docExpr.EvalName(); actual != tc.expected {
+			t.Errorf("%s: got %#v, expected %#v", k, actual, tc.expected)
+		}
+	}
+}
+func TestContactExprEvalName(t *testing.T) {
+	cases := map[string]struct {
+		name     string
+		email    string
+		url      string
+		expected string
+	}{
+		"test, only with name":   {name: "parrot", expected: "Contact parrot"},
+		"test, name and email":   {name: "parrot", email: "parrot@gopher.com", expected: "Contact parrot"},
+		"test, name and url":     {name: "parrot", url: "https://parrot.com", expected: "Contact parrot"},
+		"test, name, url, email": {name: "parrot", email: "parrot@gopher.com", url: "https://parrot.com", expected: "Contact parrot"},
+		"test, empty name":       {name: "", expected: "Contact "},
+	}
+	for k, tc := range cases {
+		contactExpr := ContactExpr{Name: tc.name, Email: tc.email, URL: tc.url}
+		if actual := contactExpr.EvalName(); actual != tc.expected {
+			t.Errorf("%s: got %#v, expected %#v", k, actual, tc.expected)
+		}
+	}
+}
 func TestLicenseExprEvalName(t *testing.T) {
 	cases := map[string]struct {
 		name     string
