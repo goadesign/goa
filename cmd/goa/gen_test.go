64b794cf952e05a188b03ceda1da1a82bf7c97a8
ikawaha <ikawaha@users.noreply.github.com>
2019-07-09 00:07:12 +0900

    Add mod regexp tests

--- cmd/goa/gen_test.go
+++ cmd/goa/gen_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -146,5 +147,64 @@ require (
 				t.Errorf("expected %v, got %v", c.Expected, pkg)
 			}
 		})
+	}
+}
+
+func TestModPatternRegexp(t *testing.T) {
+	cases := []struct {
+		Name     string
+		Input    string
+		Expected []string
+	}{
+		{
+			Name:     "goa v3 release version",
+			Input:    "goa.design/goa/v3 v3.0.2",
+			Expected: []string{"goa.design/goa/v3 v3.0.2", "goa.design/goa/v3", "v3.0.2"},
+		},
+		{
+			Name:     "goa v3 commit number",
+			Input:    "goa.design/goa/v3 02c84c9b39d845611bfe6e8a96465d698ea374b1",
+			Expected: []string{"goa.design/goa/v3 02c84c9b39d845611bfe6e8a96465d698ea374b1", "goa.design/goa/v3", "02c84c9b39d845611bfe6e8a96465d698ea374b1"},
+		},
+		{
+			Name:     "goa v3 with comment",
+			Input:    "goa.design/goa/v3   v3.0.2 //comment1 //comment2 //comment3",
+			Expected: []string{"goa.design/goa/v3   v3.0.2 //comment1 //comment2 //comment3", "goa.design/goa/v3", "v3.0.2"},
+		},
+		{
+			Name:     "goa v3 with require and comment",
+			Input:    "require goa.design/goa/v3 v3.0.2 //comment",
+			Expected: []string{"require goa.design/goa/v3 v3.0.2 //comment", "goa.design/goa/v3", "v3.0.2"},
+		},
+		{
+			Name:     "goa v2",
+			Input:    "goa.design/goa/v2 v2.0.0",
+			Expected: []string{"goa.design/goa/v2 v2.0.0", "goa.design/goa/v2", "v2.0.0"},
+		},
+		{
+			Name:     "goa v123",
+			Input:    "   goa.design/goa/v123    v123.4.5   ",
+			Expected: []string{"   goa.design/goa/v123    v123.4.5   ", "goa.design/goa/v123", "v123.4.5"},
+		},
+		{
+			Name:     "without version",
+			Input:    "goa.design/goa/v3",
+			Expected: nil,
+		},
+		{
+			Name:     "comment out",
+			Input:    "// goa.design/goa/v3 v3.0.0",
+			Expected: nil,
+		},
+		{
+			Name:     "irrelevant package",
+			Input:    "github.com/ikawaha/kagome v1.10.0",
+			Expected: nil,
+		},
+	}
+	for _, c := range cases {
+		if got := reMod.FindStringSubmatch(c.Input); !reflect.DeepEqual(c.Expected, got) {
+			t.Errorf("expected %+v, got %+v", c.Expected, got)
+		}
 	}
 }
