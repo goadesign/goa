1bd4d228f6eb3fdb3f0bd6b34394d74e783649f3
Raphael Simon <simon.raphael@gmail.com>
2019-09-08 19:07:12 +0100

    Fix path to encoding docs
    in generated example comments.

--- http/codegen/example_server.go
+++ http/codegen/example_server.go
@@ -217,7 +217,7 @@ func handleHTTPServer(ctx context.Context, u *url.URL{{ range $.Services }}{{ if
 	// Provide the transport specific request decoder and response encoder.
 	// The goa http package has built-in support for JSON, XML and gob.
 	// Other encodings can be used by providing the corresponding functions,
-	// see goa.design/encoding.
+	// see goa.design/implement/encoding.
 	var (
 		dec = goahttp.RequestDecoder
 		enc = goahttp.ResponseEncoder
