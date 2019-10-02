86298c13ffe60c45eb2f87100728f9518e4575a3
Taichi Sasaki <tchsskk@gmail.com>
2019-10-02 22:22:29 +0900

    Add tests for buildPathFromFileServer()
    in http/codegen/openapi.

---
+++ http/codegen/openapi/openapi_v2_builder_test.go
@@ -0,0 +1,51 @@
+package openapi
+
+import (
+	"testing"
+
+	"goa.design/goa/expr"
+)
+
+func TestBuildPathFromFileServer(t *testing.T) {
+	cases := []struct {
+		path     string
+		expected string
+	}{
+		{
+			path:     "/foo",
+			expected: "/foo",
+		},
+		{
+			path:     "/foo/{bar}",
+			expected: "/foo/{bar}",
+		},
+		{
+			path:     "/foo/{*bar}",
+			expected: "/foo/{bar}",
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.path, func(t *testing.T) {
+			s := &V2{
+				Paths: make(map[string]interface{}),
+			}
+			root := &expr.RootExpr{
+				API: &expr.APIExpr{},
+			}
+			fs := &expr.HTTPFileServerExpr{
+				Service: &expr.HTTPServiceExpr{
+					ServiceExpr: &expr.ServiceExpr{
+						Name: "service",
+					},
+				},
+				RequestPaths: []string{tc.path},
+			}
+			buildPathFromFileServer(s, root, fs)
+			for actual := range s.Paths {
+				if actual != tc.expected {
+					t.Errorf("got %#v, expected %#v", actual, tc.expected)
+				}
+			}
+		})
+	}
+}
