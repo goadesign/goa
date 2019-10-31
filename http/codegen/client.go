ebdc1bde19425fb55fee780e745dc38247524f52
ikawaha <ikawaha@users.noreply.github.com>
2019-10-31 15:24:08 +0900

    Fix composite literal uses unkeyed fields

--- http/codegen/client.go
+++ http/codegen/client.go
@@ -608,7 +608,7 @@ func {{ .ResponseDecoder }}(decoder func(*http.Response) goahttp.Decoder, restor
 				{{- else }}
 			view := resp.Header.Get("goa-view")
 				{{- end }}
-			vres := {{ if not $.Method.ViewedResult.IsCollection }}&{{ end }}{{ $.Method.ViewedResult.ViewsPkg}}.{{ $.Method.ViewedResult.VarName }}{p, view}
+			vres := {{ if not $.Method.ViewedResult.IsCollection }}&{{ end }}{{ $.Method.ViewedResult.ViewsPkg}}.{{ $.Method.ViewedResult.VarName }}{Projected: p, View: view}
 			{{- if .ClientBody }}
 				if err = {{ $.Method.ViewedResult.ViewsPkg}}.Validate{{ $.Method.Result }}(vres); err != nil {
 					return nil, goahttp.ErrValidationError("{{ $.ServiceName }}", "{{ $.Method.Name }}", err)
