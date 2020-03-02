54f1e569c3e4a8d54306cea9d2408a5e210e9cc6
Raphael Simon <simon.raphael@gmail.com>
2020-03-01 20:48:57 -0800

    Properly handle static view usage (#2507)
    When combined with dynamic view for same result type in same service.

--- codegen/service/service_data.go
+++ codegen/service/service_data.go
@@ -1593,10 +1593,20 @@ func walkViewAttrs(obj *expr.Object, view *expr.ViewExpr, walker func(name strin
 }
 
 const (
-	initTypeCodeT = `{{- if or .ToResult .ToViewed -}}
+	initTypeCodeT = `{{ if or .ToResult .ToViewed }}
+	{{- if eq (len .Views) 1 }}
+		{{- with (index .Views 0) }}
+			{{- if $.ToViewed -}}
+	p := {{ $.InitName }}{{ if ne .Name "default" }}{{ goify .Name true }}{{ end }}({{ $.ArgVar }})
+	return {{ if not $.IsCollection }}&{{ end }}{{ $.TargetType }}{Projected: p, View: {{ printf "%q" .Name }} }
+ 			{{- else -}}
+			return {{ $.InitName }}{{ if ne .Name "default" }}{{ goify .Name true }}{{ end }}({{ $.ArgVar }}.Projected)
+			{{- end }}
+		{{- end }}
+	{{- else -}}
 	var {{ .ReturnVar }} {{ .ReturnTypeRef }}
 	switch {{ if .ToResult }}{{ .ArgVar }}.View{{ else }}view{{ end }} {
-	{{- range .Views }}
+		{{- range .Views }}
 		case {{ printf "%q" .Name }}{{ if eq .Name "default" }}, ""{{ end }}:
 			{{- if $.ToViewed }}
 				p := {{ $.InitName }}{{ if ne .Name "default" }}{{ goify .Name true }}{{ end }}({{ $.ArgVar }})
@@ -1604,13 +1614,16 @@ const (
 			{{- else }}
 				{{ $.ReturnVar }} = {{ $.InitName }}{{ if ne .Name "default" }}{{ goify .Name true }}{{ end }}({{ $.ArgVar }}.Projected)
 			{{- end }}
-	{{- end }}
+		{{- end }}
 	}
+	return {{ .ReturnVar }}
+	{{- end }}
 {{- else if .IsCollection -}}
 	{{ .ReturnVar }} := make({{ .TargetType }}, len({{ .ArgVar }}))
 	for i, n := range {{ .ArgVar }} {
 		{{ .ReturnVar }}[i] = {{ .InitName }}(n)
 	}
+	return {{ .ReturnVar }}
 {{- else -}}
 	{{ .Code }}
 	{{- range .Fields }}
@@ -1618,8 +1631,8 @@ const (
 			{{ $.Target }}.{{ .VarName }} = {{ .FieldInit }}({{ $.Source }}.{{ .VarName }})
 		}
 	{{- end }}
-{{- end }}
-return {{ .ReturnVar }}`
+	return {{ .ReturnVar }}
+{{- end }}`
 
 	validateTypeT = `{{- if .IsViewed -}}
 switch {{ .ArgVar }}.View {
