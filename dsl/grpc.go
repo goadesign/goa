0b5895b3ee2b6f9114700b21826cc2267062de2f
Nitin Mohan <nitinmohan87@gmail.com>
2020-07-17 10:53:30 -0700

    Support GRPC error responses in API (#2610)

--- dsl/grpc.go
+++ dsl/grpc.go
@@ -58,7 +58,7 @@ const (
 // appear in the request or response types. The functions may also define new
 // attributes or override the existing request or response type attributes.
 //
-// GRPC must appear in a Service or a Method expression.
+// GRPC must appear in an API, a Service, or a Method expression.
 //
 // GRPC accepts a single argument which is the defining DSL function.
 //
@@ -92,6 +92,8 @@ const (
 //
 func GRPC(fn func()) {
 	switch actual := eval.Current().(type) {
+	case *expr.APIExpr:
+		eval.Execute(fn, expr.Root.API.GRPC)
 	case *expr.ServiceExpr:
 		res := expr.Root.API.GRPC.ServiceFor(actual)
 		res.DSLFunc = fn
