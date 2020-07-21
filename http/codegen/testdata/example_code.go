eee3ca1532765d50c40fc0eecd99d5862fd2ae69
hirosassa <hiro.sassa@gmail.com>
2020-07-22 08:36:48 +0900

    lint errcheck

--- http/codegen/testdata/example_code.go
+++ http/codegen/testdata/example_code.go
@@ -81,7 +81,7 @@ func handleHTTPServer(ctx context.Context, u *url.URL, serviceEndpoints *service
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
 
-		srv.Shutdown(ctx)
+		_ = srv.Shutdown(ctx)
 	}()
 }
 
@@ -91,7 +91,7 @@ func handleHTTPServer(ctx context.Context, u *url.URL, serviceEndpoints *service
 func errorHandler(logger *log.Logger) func(context.Context, http.ResponseWriter, error) {
 	return func(ctx context.Context, w http.ResponseWriter, err error) {
 		id := ctx.Value(middleware.RequestIDKey).(string)
-		w.Write([]byte("[" + id + "] encoding: " + err.Error()))
+		_, _ = w.Write([]byte("[" + id + "] encoding: " + err.Error()))
 		logger.Printf("[%s] ERROR: %s", id, err.Error())
 	}
 }
