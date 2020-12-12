24de5c4a71dfb37e2374b0239944ed0f36557a4f
Nitin Mohan <nitinmohan87@gmail.com>
2020-12-11 17:58:23 -0800

    Openapi v3 response fixes (#2737) (#2740)
    * Set default response description in openapi v3
    
    * Remove content for empty response body types in openapi v3

--- http/codegen/openapi/v3/response.go
+++ http/codegen/openapi/v3/response.go
@@ -1,6 +1,9 @@
 package openapiv3
 
 import (
+	"fmt"
+	"net/http"
+
 	"goa.design/goa/expr"
 	"goa.design/goa/http/codegen/openapi"
 )
@@ -30,15 +33,26 @@ func responseFromExpr(r *expr.HTTPResponseExpr, bodies map[int][]*openapi.Schema
 			return nil
 		})
 	}
-	mt := &MediaType{
-		Schema:     bodies[r.StatusCode][0],
-		Example:    r.Body.Example(rand),
-		Extensions: openapi.ExtensionsFromExpr(r.Body.Meta),
+
+	var content map[string]*MediaType
+	{
+		if r.Body.Type != expr.Empty {
+			content = make(map[string]*MediaType)
+			content[ct] = &MediaType{
+				Schema:     bodies[r.StatusCode][0],
+				Example:    r.Body.Example(rand),
+				Extensions: openapi.ExtensionsFromExpr(r.Body.Meta),
+			}
+		}
+	}
+	desc := r.Description
+	if desc == "" {
+		desc = fmt.Sprintf("%s response.", http.StatusText(r.StatusCode))
 	}
 	return &Response{
-		Description: &r.Description,
+		Description: &desc,
 		Headers:     headers,
-		Content:     map[string]*MediaType{ct: mt},
+		Content:     content,
 		Extensions:  openapi.ExtensionsFromExpr(r.Meta),
 	}
 }
