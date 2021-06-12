f78e814ab9e3eae8f9289789cbb2a237bc1ab50e
Taichi Sasaki <tchsskk@gmail.com>
2021-06-12 11:39:38 +0900

    Fix a test name

--- http/codegen/server_error_encoder_test.go
+++ http/codegen/server_error_encoder_test.go
@@ -15,7 +15,7 @@ func TestEncodeError(t *testing.T) {
 		Code string
 	}{
 		{"primitive-error-response", testdata.PrimitiveErrorResponseDSL, testdata.PrimitiveErrorResponseEncoderCode},
-		{"primitive-error-response", testdata.PrimitiveErrorInResponseHeaderDSL, testdata.PrimitiveErrorInResponseHeaderEncoderCode},
+		{"primitive-error-in-response-header", testdata.PrimitiveErrorInResponseHeaderDSL, testdata.PrimitiveErrorInResponseHeaderEncoderCode},
 		{"default-error-response", testdata.DefaultErrorResponseDSL, testdata.DefaultErrorResponseEncoderCode},
 		{"default-error-response-with-content-type", testdata.DefaultErrorResponseWithContentTypeDSL, testdata.DefaultErrorResponseWithContentTypeEncoderCode},
 		{"service-error-response", testdata.ServiceErrorResponseDSL, testdata.ServiceErrorResponseEncoderCode},
