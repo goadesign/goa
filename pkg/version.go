ea810e8a6dd18f08d0eea276324f6d4179050ff8
Raphael Simon <simon.raphael@gmail.com>
2021-09-04 14:22:34 -0700

    Release v3.5.0

--- pkg/version.go
+++ pkg/version.go
@@ -10,9 +10,9 @@ const (
 	// Major version number
 	Major = 3
 	// Minor version number
-	Minor = 4
+	Minor = 5
 	// Build number
-	Build = 3
+	Build = 0
 	// Suffix - set to empty string in release tag commits.
 	Suffix = ""
 )
