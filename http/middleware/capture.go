617b756f8c090fc47ec13afa0a5aaf93f7ec7f48
Raphael Simon <simon.raphael@gmail.com>
2021-11-30 13:44:43 -0800

    Properly handle hijacked connection in ResponseCapture middleware

--- http/middleware/capture.go
+++ http/middleware/capture.go
@@ -13,6 +13,7 @@ type ResponseCapture struct {
 	http.ResponseWriter
 	StatusCode    int
 	ContentLength int
+	Hijacked      bool
 }
 
 // CaptureResponse creates a ResponseCapture that wraps the given ResponseWriter.
@@ -22,12 +23,18 @@ func CaptureResponse(w http.ResponseWriter) *ResponseCapture {
 
 // WriteHeader records the value of the status code before writing it.
 func (w *ResponseCapture) WriteHeader(code int) {
+	if w.Hijacked {
+		return
+	}
 	w.StatusCode = code
 	w.ResponseWriter.WriteHeader(code)
 }
 
 // Write computes the written len and stores it in ContentLength.
 func (w *ResponseCapture) Write(b []byte) (int, error) {
+	if w.Hijacked {
+		return 0, nil
+	}
 	n, err := w.ResponseWriter.Write(b)
 	w.ContentLength += n
 	return n, err
@@ -36,6 +43,7 @@ func (w *ResponseCapture) Write(b []byte) (int, error) {
 // Hijack supports the http.Hijacker interface.
 func (w *ResponseCapture) Hijack() (net.Conn, *bufio.ReadWriter, error) {
 	if h, ok := w.ResponseWriter.(http.Hijacker); ok {
+		w.Hijacked = true
 		return h.Hijack()
 	}
 	return nil, nil, fmt.Errorf("response writer does not support hijacking: %T", w.ResponseWriter)
