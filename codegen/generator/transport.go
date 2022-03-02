ec818829312392642fc18933a1ae9a427f6b7f20
Raphael Simon <simon.raphael@gmail.com>
2022-03-01 20:19:26 -0800

    Do not fail on designs with no service (#3016)
    So that plugins that don't need service definitions can work properly.

--- codegen/generator/transport.go
+++ codegen/generator/transport.go
@@ -1,8 +1,6 @@
 package generator
 
 import (
-	"fmt"
-
 	"goa.design/goa/v3/codegen"
 	"goa.design/goa/v3/codegen/service"
 	"goa.design/goa/v3/eval"
@@ -45,9 +43,6 @@ func Transport(genpkg string, roots []eval.Root) ([]*codegen.File, error) {
 				}
 			}
 		}
-	}
-	if len(files) == 0 {
-		return nil, fmt.Errorf("transport: no HTTP/gRPC design found")
 	}
 	return files, nil
 }
