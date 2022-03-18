bc5e78e1f19ebedf56a190d681212e41270bf0b6
Raphael Simon <simon.raphael@gmail.com>
2022-03-17 21:32:50 -0700

    Make tests work on Windows

--- codegen/go_transform_union_test.go
+++ codegen/go_transform_union_test.go
@@ -1,18 +1,10 @@
 package codegen
 
 import (
-	"flag"
-	"io/ioutil"
-	"path/filepath"
-	"strings"
 	"testing"
 
 	"goa.design/goa/v3/codegen/testdata"
 	"goa.design/goa/v3/expr"
-)
-
-var (
-	update = flag.Bool("update", false, "update the golden files of this test")
 )
 
 func TestGoTransformUnion(t *testing.T) {
@@ -29,20 +21,20 @@ func TestGoTransformUnion(t *testing.T) {
 		defaultCtx     = NewAttributeContext(false, false, true, "", scope)
 	)
 	tc := []struct {
-		Name   string
-		Source *expr.AttributeExpr
-		Target *expr.AttributeExpr
-		Error  string
+		Name     string
+		Source   *expr.AttributeExpr
+		Target   *expr.AttributeExpr
+		Expected string
 	}{
-		{"UnionString to UnionString2", unionString, unionString2, ""},
+		{"UnionString to UnionString2", unionString, unionString2, unionToUnionCode},
 
-		{"UnionString to User Type", unionString, userType, ""},
-		{"UnionStringInt to User Type", unionStringInt, userType, ""},
-		{"UnionSomeType to User Type", unionSomeType, userType, ""},
+		{"UnionString to User Type", unionString, userType, unionStringToUserTypeCode},
+		{"UnionStringInt to User Type", unionStringInt, userType, unionStringIntToUserTypeCode},
+		{"UnionSomeType to User Type", unionSomeType, userType, unionSomeTypeToUserTypeCode},
 
-		{"User Type to UnionString", userType, unionString, ""},
-		{"User Type to UnionStringInt", userType, unionStringInt, ""},
-		{"User Type to UnionSomeType", userType, unionSomeType, ""},
+		{"User Type to UnionString", userType, unionString, userTypeToUnionStringCode},
+		{"User Type to UnionStringInt", userType, unionStringInt, userTypeToUnionStringIntCode},
+		{"User Type to UnionSomeType", userType, unionSomeType, userTypeToUnionSomeTypeCode},
 	}
 	for _, c := range tc {
 		t.Run(c.Name, func(t *testing.T) {
@@ -52,20 +44,8 @@ func TestGoTransformUnion(t *testing.T) {
 				return
 			}
 			code = FormatTestCode(t, "package foo\nfunc transform(){\n"+code+"}")
-			path := filepath.Join("testdata", strings.Replace(c.Name, " ", "_", -1))
-			if *update {
-				if err := ioutil.WriteFile(path+".golden", []byte(code), 0644); err != nil {
-					t.Error(err)
-				}
-				return
-			}
-			expected, err := ioutil.ReadFile(path + ".golden")
-			if err != nil {
-				t.Error(err)
-				return
-			}
-			if code != string(expected) {
-				t.Errorf("invalid code, got:\n%s\ngot vs. expected:\n%s", code, Diff(t, code, string(expected)))
+			if code != c.Expected {
+				t.Errorf("invalid code, got:\n%s\ngot vs. expected:\n%s", code, Diff(t, code, c.Expected))
 			}
 		})
 	}
@@ -104,3 +84,86 @@ func TestGoTransformUnionError(t *testing.T) {
 		})
 	}
 }
+
+const unionToUnionCode = `func transform() {
+	var target *UnionString2
+	switch actual := source.(type) {
+	case UnionStringString:
+		val := UnionString2String(actual)
+		target = UnionString2{Value: val}
+	}
+}
+`
+
+const unionStringToUserTypeCode = `func transform() {
+	var target *UnionUserType
+	var name string
+	switch source.(type) {
+	case UnionStringString:
+		name = "UnionStringString"
+	}
+	target = &UnionUserType{
+		Type:  name,
+		Value: source,
+	}
+}
+`
+
+const unionStringIntToUserTypeCode = `func transform() {
+	var target *UnionUserType
+	var name string
+	switch source.(type) {
+	case UnionStringIntString:
+		name = "UnionStringIntString"
+	case UnionStringIntInt:
+		name = "UnionStringIntInt"
+	}
+	target = &UnionUserType{
+		Type:  name,
+		Value: source,
+	}
+}
+`
+
+const unionSomeTypeToUserTypeCode = `func transform() {
+	var target *UnionUserType
+	var name string
+	switch source.(type) {
+	case *SomeType:
+		name = "SomeType"
+	}
+	target = &UnionUserType{
+		Type:  name,
+		Value: source,
+	}
+}
+`
+
+const userTypeToUnionStringCode = `func transform() {
+	var target *UnionString
+	switch source.Type {
+	case "UnionStringString":
+		target = source.Value.(UnionStringString)
+	}
+}
+`
+
+const userTypeToUnionStringIntCode = `func transform() {
+	var target *UnionStringInt
+	switch source.Type {
+	case "UnionStringIntString":
+		target = source.Value.(UnionStringIntString)
+	case "UnionStringIntInt":
+		target = source.Value.(UnionStringIntInt)
+	}
+}
+`
+
+const userTypeToUnionSomeTypeCode = `func transform() {
+	var target *UnionSomeType
+	switch source.Type {
+	case "SomeType":
+		target = source.Value.(*SomeType)
+	}
+}
+`
