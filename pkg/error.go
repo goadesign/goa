cf2705dec63c8ca662922c6c593d31f1cc025315
ikawaha <ikawaha@users.noreply.github.com>
2022-05-02 17:08:40 +0900

    Add structure for chaining errors

--- pkg/error.go
+++ pkg/error.go
@@ -3,6 +3,7 @@ package goa
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
@@ -256,4 +257,53 @@ func asError(err error) *ServiceError {
 		}
 	}
 	return e
+}
+
+type errChain struct {
+	err  error
+	next error
+}
+
+func newErrChain(err error) error {
+	if err == nil {
+		return nil
+	}
+	if errors.As(err, &errChain{}) {
+		return err
+	}
+	return &errChain{err: err}
+}
+
+// Error implements error interface.
+func (e errChain) Error() string {
+	return e.err.Error()
+}
+
+// Is reports whether any error in err's chain matches target.
+func (e errChain) Is(err error) bool {
+	return errors.Is(e.err, err)
+}
+
+// As reports whether any error in err's chain matches target type.
+func (e errChain) As(target interface{}) bool {
+	return errors.As(e.err, target)
+}
+
+// Unwrap implements Wrapper interface.
+func (e errChain) Unwrap() error {
+	return e.next
+}
+
+// Append appends errors and obtains chained errors.
+func Append(lhs error, rhs ...error) error {
+	if len(rhs) == 0 || rhs[0] == nil {
+		return newErrChain(lhs)
+	}
+	if lhs == nil {
+		return Append(rhs[0], rhs[1:]...)
+	}
+	if len(rhs) == 1 {
+		return &errChain{err: lhs, next: newErrChain(rhs[0])}
+	}
+	return Append(lhs, Append(rhs[0], rhs[1:]...))
 }
