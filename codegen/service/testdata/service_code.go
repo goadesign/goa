goadesign/goa

commit 683efb2f14ccc36a6378baf0a6808fcda5ed0ade

pkg:path types that reference their own package types (#3022)
* pkg:path types that reference their own package types

If you have a user type placed in a package, ie. common, and it has a
field of another type in package common, we codegen a type reference of
common.Thing which will fail, as that code is already in the common
package.

* Fix codegen for custom package user types

with attributes that themselves use the custom package path.

Co-authored-by: Raphael Simon <simon.raphael@gmail.com>

codegen/scope.go
@@ -96,6 +96,10 @@ func (s *NameScope) Name(name string) string {
 // useDefault if true indicates that the attribute must not be a pointer
 // if it has a default value.
 func (s *NameScope) GoTypeDef(att *expr.AttributeExpr, ptr, useDefault bool) string {
+	return s.goTypeDef(att, ptr, useDefault, "")
+}
+
+func (s *NameScope) goTypeDef(att *expr.AttributeExpr, ptr, useDefault bool, pkg string) string {
 	switch actual := att.Type.(type) {
 	case expr.Primitive:
 		if t, _ := GetMetaType(att); t != "" {
@@ -103,17 +107,17 @@ func (s *NameScope) GoTypeDef(att *expr.AttributeExpr, ptr, useDefault bool) str
 		}
 		return GoNativeTypeName(actual)
 	case *expr.Array:
-		d := s.GoTypeDef(actual.ElemType, ptr, useDefault)
+		d := s.goTypeDef(actual.ElemType, ptr, useDefault, pkg)
 		if expr.IsObject(actual.ElemType.Type) {
 			d = "*" + d
 		}
 		return "[]" + d
 	case *expr.Map:
-		keyDef := s.GoTypeDef(actual.KeyType, ptr, useDefault)
+		keyDef := s.goTypeDef(actual.KeyType, ptr, useDefault, pkg)
 		if expr.IsObject(actual.KeyType.Type) {
 			keyDef = "*" + keyDef
 		}
-		elemDef := s.GoTypeDef(actual.ElemType, ptr, useDefault)
+		elemDef := s.goTypeDef(actual.ElemType, ptr, useDefault, pkg)
 		if expr.IsObject(actual.ElemType.Type) {
 			elemDef = "*" + elemDef
 		}
@@ -133,7 +137,13 @@ func (s *NameScope) GoTypeDef(att *expr.AttributeExpr, ptr, useDefault bool) str
 			)
 			{
 				fn = GoifyAtt(at, name, true)
-				tdef = s.GoTypeDef(at, ptr, useDefault)
+				var parentPkg string
+				if ut, ok := at.Type.(expr.UserType); ok {
+					if UserTypeLocation(ut) != nil {
+						parentPkg = UserTypeLocation(ut).PackageName()
+					}
+				}
+				tdef = s.goTypeDef(at, ptr, useDefault, parentPkg)
 				if expr.IsObject(at.Type) ||
 					att.IsPrimitivePointer(name, useDefault) ||
 					(ptr && expr.IsPrimitive(at.Type) && at.Type.Kind() != expr.AnyKind && at.Type.Kind() != expr.BytesKind) {
@@ -150,7 +160,7 @@ func (s *NameScope) GoTypeDef(att *expr.AttributeExpr, ptr, useDefault bool) str
 		return strings.Join(ss, "\n")
 	case expr.UserType:
 		var prefix string
-		if loc := UserTypeLocation(actual); loc != nil {
+		if loc := UserTypeLocation(actual); loc != nil && loc.PackageName() != pkg {
 			prefix = loc.PackageName() + "."
 		}
 		return prefix + s.GoTypeName(att)

codegen/service/service_test.go
@@ -69,6 +69,7 @@ func TestService(t *testing.T) {
 
 func TestStructPkgPath(t *testing.T) {
 	fooPath := filepath.Join("gen", "foo", "foo.go")
+	recursiveFooPath := filepath.Join("gen", "foo", "recursive_foo.go")
 	barPath := filepath.Join("gen", "bar", "bar.go")
 	bazPath := filepath.Join("gen", "baz", "baz.go")
 	cases := []struct {
@@ -80,6 +81,7 @@ func TestStructPkgPath(t *testing.T) {
 	}{
 		{"none", testdata.SingleMethodDSL, []string{testdata.SingleMethod}, nil, nil},
 		{"single", testdata.PkgPathDSL, []string{testdata.PkgPath}, []string{fooPath}, []string{testdata.PkgPathFoo}},
+		{"recursive", testdata.PkgPathRecursiveDSL, []string{testdata.PkgPathRecursive}, []string{fooPath, recursiveFooPath}, []string{testdata.PkgPathRecursiveFooFoo, testdata.PkgPathRecursiveFoo}},
 		{"multiple", testdata.PkgPathMultipleDSL, []string{testdata.PkgPathMultiple}, []string{barPath, bazPath}, []string{testdata.PkgPathBar, testdata.PkgPathBaz}},
 		{"nopkg", testdata.PkgPathNoDirDSL, []string{testdata.PkgPathNoDir}, nil, nil},
 		{"dupes", testdata.PkgPathDupeDSL, []string{testdata.PkgPathDupe1, testdata.PkgPathDupe2}, []string{fooPath}, []string{testdata.PkgPathFooDupe}},
@@ -88,7 +90,7 @@ func TestStructPkgPath(t *testing.T) {
 		t.Run(c.Name, func(t *testing.T) {
 			userTypePkgs := make(map[string][]string)
 			codegen.RunDSLWithFunc(t, c.DSL, func() {
-				expr.Root.Types = []expr.UserType{testdata.APayload, testdata.AResult, testdata.Foo, testdata.Bar, testdata.Baz, testdata.NoDir}
+				expr.Root.Types = []expr.UserType{testdata.APayload, testdata.AResult, testdata.RecursiveFoo, testdata.Foo, testdata.Bar, testdata.Baz, testdata.NoDir}
 			})
 			if len(expr.Root.Services) != len(c.SvcCodes) {
 				t.Fatalf("got %d services, expected %d", len(expr.Root.Services), len(c.SvcCodes))

codegen/service/testdata/service_code.go
@@ -2392,6 +2392,24 @@ const ServiceName = "PkgPathMethod"
 var MethodNames = [1]string{"A"}
 `
 
+const PkgPathRecursive = `
+// Service is the PkgPathRecursiveMethod service interface.
+type Service interface {
+	// A implements A.
+	A(context.Context, *foo.RecursiveFoo) (res *foo.RecursiveFoo, err error)
+}
+
+// ServiceName is the name of the service as defined in the design. This is the
+// same value that is set in the endpoint request contexts under the ServiceKey
+// key.
+const ServiceName = "PkgPathRecursiveMethod"
+
+// MethodNames lists the service method names as defined in the design. These
+// are the same values that are set in the endpoint request contexts under the
+// MethodKey key.
+var MethodNames = [1]string{"A"}
+`
+
 const PkgPathMultiple = `
 // Service is the MultiplePkgPathMethod service interface.
 type Service interface {
@@ -2416,19 +2434,32 @@ var MethodNames = [3]string{"A", "B", "EnvelopedB"}
 // EnvelopedBPayload is the payload type of the MultiplePkgPathMethod service
 // EnvelopedB method.
 type EnvelopedBPayload struct {
-	Baz *baz.Baz
+	Baz *Baz
 }
 
 // EnvelopedBResult is the result type of the MultiplePkgPathMethod service
 // EnvelopedB method.
 type EnvelopedBResult struct {
-	Baz *baz.Baz
+	Baz *Baz
 }
 `
 
 const PkgPathFoo = `// Foo is the payload type of the PkgPathMethod service A method.
 type Foo struct {
 	IntField *int
+}
+`
+
+const PkgPathRecursiveFooFoo = `
+type Foo struct {
+	IntField *int
+}
+`
+
+const PkgPathRecursiveFoo = `// RecursiveFoo is the payload type of the PkgPathRecursiveMethod service A
+// method.
+type RecursiveFoo struct {
+	Foo *Foo
 }
 `
 

codegen/service/testdata/service_dsls.go
@@ -612,6 +612,11 @@ var Foo = Type("Foo", func() {
 	Meta("struct:pkg:path", "foo")
 })
 
+var RecursiveFoo = Type("RecursiveFoo", func() {
+	Attribute("Foo", Foo)
+	Meta("struct:pkg:path", "foo")
+})
+
 var Bar = Type("Bar", func() {
 	Attribute("IntField", Int)
 	Meta("struct:pkg:path", "bar")
@@ -632,6 +637,15 @@ var PkgPathDSL = func() {
 		Method("A", func() {
 			Payload(Foo)
 			Result(Foo)
+		})
+	})
+}
+
+var PkgPathRecursiveDSL = func() {
+	Service("PkgPathRecursiveMethod", func() {
+		Method("A", func() {
+			Payload(RecursiveFoo)
+			Result(RecursiveFoo)
 		})
 	})
 }

commit 83d283b169a983b02574bb666a4479909a3003dd

Fixes #3083: properly generate attributes with meta `struct:pkg:path`
What was the issue?

What was meant to be a check on the parentPkg to avoid recursive
definitions wasn't actually checking the parent package, but the package
of the field itself.

It was a bug that got introduced in #3022 and never worked as expected.

Solution:

Injecting the parent package for the initial definition seems to address
this.

codegen/scope.go
@@ -96,7 +96,13 @@ func (s *NameScope) Name(name string) string {
 // useDefault if true indicates that the attribute must not be a pointer
 // if it has a default value.
 func (s *NameScope) GoTypeDef(att *expr.AttributeExpr, ptr, useDefault bool) string {
-	return s.goTypeDef(att, ptr, useDefault, "")
+	pkg := ""
+	if loc := UserTypeLocation(att.Type); loc != nil {
+		pkg = loc.PackageName()
+	} else if p, ok := att.Meta.Last("struct:pkg:path"); ok && p != "" {
+		pkg = p
+	}
+	return s.goTypeDef(att, ptr, useDefault, pkg)
 }
 
 func (s *NameScope) goTypeDef(att *expr.AttributeExpr, ptr, useDefault bool, pkg string) string {
@@ -138,13 +144,11 @@ func (s *NameScope) goTypeDef(att *expr.AttributeExpr, ptr, useDefault bool, pkg
 			)
 			{
 				fn = GoifyAtt(at, name, true)
-				var parentPkg string
-				if ut, ok := at.Type.(expr.UserType); ok {
-					if UserTypeLocation(ut) != nil {
-						parentPkg = UserTypeLocation(ut).PackageName()
-					}
-				}
-				tdef = s.goTypeDef(at, ptr, useDefault, parentPkg)
+				tdef = s.goTypeDef(at, ptr, useDefault, pkg)
+				// if name == "Foo" {
+				// 	tdef += "XXX"
+				// 	// panic(at)
+				// }
 				if expr.IsObject(at.Type) ||
 					att.IsPrimitivePointer(name, useDefault) ||
 					(ptr && expr.IsPrimitive(at.Type) && at.Type.Kind() != expr.AnyKind && at.Type.Kind() != expr.BytesKind) {

codegen/service/service_test.go
@@ -85,6 +85,7 @@ func TestStructPkgPath(t *testing.T) {
 		{"multiple", testdata.PkgPathMultipleDSL, []string{testdata.PkgPathMultiple}, []string{barPath, bazPath}, []string{testdata.PkgPathBar, testdata.PkgPathBaz}},
 		{"nopkg", testdata.PkgPathNoDirDSL, []string{testdata.PkgPathNoDir}, nil, nil},
 		{"dupes", testdata.PkgPathDupeDSL, []string{testdata.PkgPathDupe1, testdata.PkgPathDupe2}, []string{fooPath}, []string{testdata.PkgPathFooDupe}},
+		{"payload_attribute", testdata.PkgPathPayloadAttributeDSL, []string{testdata.PkgPathPayloadAttribute}, []string{fooPath}, []string{testdata.PkgPathPayloadAttributeFoo}},
 	}
 	for _, c := range cases {
 		t.Run(c.Name, func(t *testing.T) {

codegen/service/testdata/service_code.go
@@ -2508,13 +2508,13 @@ var MethodNames = [3]string{"A", "B", "EnvelopedB"}
 // EnvelopedBPayload is the payload type of the MultiplePkgPathMethod service
 // EnvelopedB method.
 type EnvelopedBPayload struct {
-	Baz *Baz
+	Baz *baz.Baz
 }
 
 // EnvelopedBResult is the result type of the MultiplePkgPathMethod service
 // EnvelopedB method.
 type EnvelopedBResult struct {
-	Baz *Baz
+	Baz *baz.Baz
 }
 `
 
@@ -2616,4 +2616,33 @@ const ServiceName = "PkgPathDupeMethod2"
 // are the same values that are set in the endpoint request contexts under the
 // MethodKey key.
 var MethodNames = [2]string{"A", "B"}
+`
+
+const PkgPathPayloadAttribute = `
+// Service is the PkgPathPayloadAttributeDSL service interface.
+type Service interface {
+	// Foo implements Foo.
+	FooEndpoint(context.Context, *Bar) (res *Bar, err error)
+}
+
+// ServiceName is the name of the service as defined in the design. This is the
+// same value that is set in the endpoint request contexts under the ServiceKey
+// key.
+const ServiceName = "PkgPathPayloadAttributeDSL"
+
+// MethodNames lists the service method names as defined in the design. These
+// are the same values that are set in the endpoint request contexts under the
+// MethodKey key.
+var MethodNames = [1]string{"Foo"}
+
+// Bar is the payload type of the PkgPathPayloadAttributeDSL service Foo method.
+type Bar struct {
+	Foo *foo.Foo
+}
+`
+
+const PkgPathPayloadAttributeFoo = `
+type Foo struct {
+	IntField *int
+}
 `

codegen/service/testdata/service_dsls.go
@@ -899,3 +899,20 @@ var PkgPathDupeDSL = func() {
 		})
 	})
 }
+
+var PkgPathPayloadAttributeDSL = func() {
+	var Foo = Type("Foo", func() {
+		Attribute("IntField", Int)
+		Meta("struct:pkg:path", "foo")
+	})
+	var Bar = Type("Bar", func() {
+		Attribute("Foo", Foo)
+	})
+
+	Service("PkgPathPayloadAttributeDSL", func() {
+		Method("Foo", func() {
+			Payload(Bar)
+			Result(Bar)
+		})
+	})
+}

codegen/types_test.go
@@ -33,7 +33,10 @@ func TestGoTypeDef(t *testing.T) {
 		stringMetaType         = expr.MetaExpr{"struct:field:type": []string{"string"}}
 		jsonWithImportMetaType = expr.MetaExpr{"struct:field:type": []string{"json.RawMessage", "encoding/json"}}
 		jsonWithRenameMetaType = expr.MetaExpr{"struct:field:type": []string{"jason.RawMessage", "encoding/json", "jason"}}
-		mixedObj               = &expr.AttributeExpr{
+		structPkgPathMetaType  = expr.MetaExpr{"struct:pkg:path": []string{"types"}}
+		utPkgPathMeta          = &expr.UserTypeExpr{AttributeExpr: &expr.AttributeExpr{Type: expr.Boolean, Meta: structPkgPathMetaType}, TypeName: "UserType"}
+
+		mixedObj = &expr.AttributeExpr{
 			Type: &expr.Object{
 				{"IntField", &expr.AttributeExpr{Type: expr.Int}},
 				{"ArrayField", simpleArray},
@@ -41,6 +44,7 @@ func TestGoTypeDef(t *testing.T) {
 				{"UserTypeField", userType},
 				{"MetaTypeField", &expr.AttributeExpr{Type: expr.Int, Meta: jsonWithImportMetaType}},
 				{"QualifiedMetaTypeField", &expr.AttributeExpr{Type: expr.Int, Meta: jsonWithRenameMetaType}},
+				{"StructPkgPath", &expr.AttributeExpr{Type: utPkgPathMeta}},
 			},
 			Validation: &expr.ValidationExpr{Required: []string{"IntField", "ArrayField", "MapField", "UserTypeField", "MetaTypeField", "QualifiedMetaTypeField"}}}
 	)
@@ -71,8 +75,8 @@ func TestGoTypeDef(t *testing.T) {
 		"Object":          {requiredObj, false, true, "struct {\n\tIntField int\n\tStringField string\n}"},
 		"ObjDefault":      {defaultObj, false, true, "struct {\n\tIntField int\n\tStringField string\n}"},
 		"ObjDefaultNoDef": {defaultObj, false, false, "struct {\n\tIntField *int\n\tStringField *string\n}"},
-		"ObjMixed":        {mixedObj, false, true, "struct {\n\tIntField int\n\tArrayField []bool\n\tMapField map[int]string\n\tUserTypeField UserType\n\tMetaTypeField json.RawMessage\n\tQualifiedMetaTypeField jason.RawMessage\n}"},
-		"ObjMixedPointer": {mixedObj, true, true, "struct {\n\tIntField *int\n\tArrayField []bool\n\tMapField map[int]string\n\tUserTypeField *UserType\n\tMetaTypeField *json.RawMessage\n\tQualifiedMetaTypeField *jason.RawMessage\n}"},
+		"ObjMixed":        {mixedObj, false, true, "struct {\n\tIntField int\n\tArrayField []bool\n\tMapField map[int]string\n\tUserTypeField UserType\n\tMetaTypeField json.RawMessage\n\tQualifiedMetaTypeField jason.RawMessage\n\tStructPkgPath *types.UserType\n}"},
+		"ObjMixedPointer": {mixedObj, true, true, "struct {\n\tIntField *int\n\tArrayField []bool\n\tMapField map[int]string\n\tUserTypeField *UserType\n\tMetaTypeField *json.RawMessage\n\tQualifiedMetaTypeField *jason.RawMessage\n\tStructPkgPath *types.UserType\n}"},
 
 		"MetaTypeSameAsDesign":                      {&expr.AttributeExpr{Type: expr.String, Meta: stringMetaType}, false, true, "string"},
 		"MetaTypeOverrideDesign":                    {&expr.AttributeExpr{Type: expr.String, Meta: jsonWithImportMetaType}, false, true, "json.RawMessage"},