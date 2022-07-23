53087004f44cde2fdd143e39f89e93e0aa867223
Raphael Simon <simon.raphael@gmail.com>
2022-07-23 16:21:48 -0700

    Add missing imports to gRPC client and server (#3102)
    when using custom package with 'struct:pkg:path' meta.

--- grpc/codegen/client.go
+++ grpc/codegen/client.go
@@ -36,17 +36,19 @@ func client(genpkg string, svc *expr.GRPCServiceExpr) *codegen.File {
 	{
 		svcName := data.Service.PathName
 		fpath = filepath.Join(codegen.Gendir, "grpc", svcName, "client", "client.go")
+		imports := []*codegen.ImportSpec{
+			{Path: "context"},
+			{Path: "google.golang.org/grpc"},
+			codegen.GoaImport(""),
+			codegen.GoaNamedImport("grpc", "goagrpc"),
+			codegen.GoaNamedImport("grpc/pb", "goapb"),
+			{Path: path.Join(genpkg, svcName), Name: data.Service.PkgName},
+			{Path: path.Join(genpkg, svcName, "views"), Name: data.Service.ViewsPkg},
+			{Path: path.Join(genpkg, "grpc", svcName, pbPkgName), Name: data.PkgName},
+		}
+		imports = append(imports, data.Service.UserTypeImports...)
 		sections = []*codegen.SectionTemplate{
-			codegen.Header(svc.Name()+" gRPC client", "client", []*codegen.ImportSpec{
-				{Path: "context"},
-				{Path: "google.golang.org/grpc"},
-				codegen.GoaImport(""),
-				codegen.GoaNamedImport("grpc", "goagrpc"),
-				codegen.GoaNamedImport("grpc/pb", "goapb"),
-				{Path: path.Join(genpkg, svcName), Name: data.Service.PkgName},
-				{Path: path.Join(genpkg, svcName, "views"), Name: data.Service.ViewsPkg},
-				{Path: path.Join(genpkg, "grpc", svcName, pbPkgName), Name: data.PkgName},
-			}),
+			codegen.Header(svc.Name()+" gRPC client", "client", imports),
 		}
 		sections = append(sections, &codegen.SectionTemplate{
 			Name:   "client-struct",
