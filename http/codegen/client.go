9ccf2f4991816db74ea50e8b720eea94f76edab1
Raphael Simon <simon.raphael@gmail.com>
2022-08-06 13:43:19 -0700

    Remove use of deprecated ioutil package

--- http/codegen/client.go
+++ http/codegen/client.go
@@ -101,7 +101,6 @@ func clientEncodeDecodeFile(genpkg string, svc *expr.HTTPServiceExpr) *codegen.F
 		{Path: "encoding/json"},
 		{Path: "fmt"},
 		{Path: "io"},
-		{Path: "io/ioutil"},
 		{Path: "mime/multipart"},
 		{Path: "net/http"},
 		{Path: "net/url"},
@@ -647,13 +646,13 @@ const responseDecoderT = `{{ printf "%s returns a decoder for responses returned
 func {{ .ResponseDecoder }}(decoder func(*http.Response) goahttp.Decoder, restoreBody bool) func(*http.Response) (interface{}, error) {
 	return func(resp *http.Response) (interface{}, error) {
 		if restoreBody {
-			b, err := ioutil.ReadAll(resp.Body)
+			b, err := io.ReadAll(resp.Body)
 			if err != nil {
 				return nil, err
 			}
-			resp.Body = ioutil.NopCloser(bytes.NewBuffer(b))
+			resp.Body = io.NopCloser(bytes.NewBuffer(b))
 			defer func() {
-				resp.Body = ioutil.NopCloser(bytes.NewBuffer(b))
+				resp.Body = io.NopCloser(bytes.NewBuffer(b))
 			}()
 		}
 		{{- if not .Method.SkipResponseBodyEncodeDecode }} else {
@@ -724,7 +723,7 @@ func {{ .ResponseDecoder }}(decoder func(*http.Response) goahttp.Decoder, restor
 				{{- end }}
 			{{- end }}
 		default:
-			body, _ := ioutil.ReadAll(resp.Body)
+			body, _ := io.ReadAll(resp.Body)
 			return nil, goahttp.ErrInvalidResponse({{ printf "%q" $.ServiceName }}, {{ printf "%q" $.Method.Name }}, resp.StatusCode, string(body))
 		}
 		{{- else }}
@@ -741,7 +740,7 @@ func {{ .ResponseDecoder }}(decoder func(*http.Response) goahttp.Decoder, restor
 		{{- end }}
 	{{- end }}
 		default:
-			body, _ := ioutil.ReadAll(resp.Body)
+			body, _ := io.ReadAll(resp.Body)
 			return nil, goahttp.ErrInvalidResponse({{ printf "%q" .ServiceName }}, {{ printf "%q" .Method.Name }}, resp.StatusCode, string(body))
 		}
 	}
@@ -950,7 +949,7 @@ func {{ .InitName }}(encoderFn {{ .FuncName }}) func(r *http.Request) goahttp.En
 			if err := encoderFn(mw, p); err != nil {
 				return err
 			}
-			r.Body = ioutil.NopCloser(body)
+			r.Body = io.NopCloser(body)
 			r.Header.Set("Content-Type", mw.FormDataContentType())
 			return mw.Close()
 		})
