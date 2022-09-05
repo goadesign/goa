92ed9d55041f1206a00cf7219ab6e66edd1ae6d6
Raphael Simon <simon.raphael@gmail.com>
2022-09-05 13:04:34 -0700

    Make middleware keys configurable
    Add ability to configure HTTP header used to propagate
    request ID.

--- middleware/ctxkeys.go
+++ middleware/ctxkeys.go
@@ -1,24 +1,19 @@
 package middleware
 
-type (
-	// private type used to define context keys
-	ctxKey int
-)
-
-const (
+var (
 	// RequestIDKey is the request context key used to store the request ID
 	// created by the RequestID middleware.
-	RequestIDKey ctxKey = iota + 1
+	RequestIDKey = "goa-request-id"
 
 	// TraceIDKey is the request context key used to store the current Trace
 	// ID if any.
-	TraceIDKey
+	TraceIDKey = "goa-trace-id"
 
 	// TraceSpanIDKey is the request context key used to store the current
 	// trace span ID if any.
-	TraceSpanIDKey
+	TraceSpanIDKey = "goa-trace-span-id"
 
 	// TraceParentSpanIDKey is the request context key used to store the current
 	// trace parent span ID if any.
-	TraceParentSpanIDKey
+	TraceParentSpanIDKey = "goa-trace-parent-span-id"
 )
