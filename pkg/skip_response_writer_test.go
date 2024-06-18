6a73729865e613b9b480f1228f4c18514d4b0f04
Jonathan Duck <duckbrain30@gmail.com>
2024-06-18 16:23:37 -0700

    Implement optional WriterTo path for writing HTTP responses

---
+++ pkg/skip_response_writer_test.go
@@ -0,0 +1,36 @@
+package goa
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+)
+
+func TestSkipResponseWriter(t *testing.T) {
+	const input = "Hello, World!"
+	var responseWriter io.ReadCloser
+
+	responseWriter = SkipResponseWriter(strings.NewReader(input))
+	defer responseWriter.Close()
+	_, ok := responseWriter.(io.WriterTo)
+	if !ok {
+		t.Errorf("SkipResponseWriter's result must implement io.WriterTo")
+	}
+
+	var writerToBuffer bytes.Buffer
+	io.Copy(&writerToBuffer, responseWriter) // io.Copy uses WriterTo if implemented
+	if writerToBuffer.String() != input {
+		t.Errorf("WriteTo: expected=%q actual=%q", input, writerToBuffer.String())
+	}
+
+	responseWriter = SkipResponseWriter(strings.NewReader(input))
+	defer responseWriter.Close()
+	readBytes, err := io.ReadAll(responseWriter) // io.ReadAll ignores WriterTo and calls Read
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(readBytes) != input {
+		t.Errorf("Read: expected=%q actual=%q", input, string(readBytes))
+	}
+}
